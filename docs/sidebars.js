add6e7270046547a124de0e0b05afb639cfb930d
kozake <kozake.shinichi@tis.co.jp>
2021-02-25 18:34:55 +0900

    modal

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -103,6 +103,7 @@ module.exports = {
             'react-native/learn/todo-app/app-hands-on/welcome',
             'react-native/learn/todo-app/app-hands-on/todo-form',
             'react-native/learn/todo-app/app-hands-on/use-focus-effect',
+            'react-native/learn/todo-app/app-hands-on/modal',
           ],
         },
       ],
