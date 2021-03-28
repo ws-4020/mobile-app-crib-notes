5055cc802892cfb58d72d58e0c7b17f3271c95bd
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-03-28 12:42:39 +0900

    💄: Add the launch screen

--- example-app/SantokuApp/src/screens/home/HomeScreen.tsx
+++ example-app/SantokuApp/src/screens/home/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {useCallback, useContext} from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, {useCallback, useContext, useEffect} from 'react';
 import {ThemeContext} from 'react-native-elements';
 
 import {HomeTemplate} from './HomeTemplate';
@@ -15,6 +16,17 @@ const Screen: React.FC = () => {
   const onGoToInstructionButtonPress = useNavigateToDemoStackScreen('Instructions');
   const onViewApplicationInformationButtonPress = useNavigateToDemoStackScreen('AppInfo');
   const onGoToDemoButtonPress = useNavigateToDemoStackScreen('Demo');
+
+  useEffect(() => {
+    // スプラッシュスクリーンを閉じる前に100ms程度は待たないと、コンポーネントのレンダリングが完了せず画面が一瞬白くなってしまいます。
+    // expo-app-loadingでも200ms待っているので、ここでも200ms待つようにしています。
+    // cf: https://github.com/expo/expo/blob/sdk-41/packages/expo-app-loading/src/AppLoadingNativeWrapper.tsx#L27
+    setTimeout(() => {
+      SplashScreen.hideAsync().catch(() => {
+        /* Ignore error */
+      });
+    }, 200);
+  }, []);
 
   return (
     <HomeTemplate
