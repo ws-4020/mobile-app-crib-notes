fa1848768e5e7a55b2a424e53ec1fbc0a219f6fc
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-05-25 02:11:15 +0900

    ✨: aタグのpropsをRepositoryLinkに追加

--- src/components/RepositoryLink.tsx
+++ src/components/RepositoryLink.tsx
@@ -1,16 +1,20 @@
 import {DocusaurusContext} from '@docusaurus/types';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import React, {PropsWithChildren} from 'react';
+import React, {ComponentProps, PropsWithChildren} from 'react';
 
 export type RepositoryLinkProps = {
   path?: string;
 };
 
-export const RepositoryLink: React.FC<PropsWithChildren<RepositoryLinkProps>> = ({path, children}) => {
+export const RepositoryLink: React.FC<PropsWithChildren<RepositoryLinkProps & ComponentProps<'a'>>> = ({
+  path,
+  children,
+  ...anchorProps
+}) => {
   const {siteConfig} = useDocusaurusContext() as DocusaurusContext;
   const href = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}${path ?? ''}`;
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a href={href} target="_blank" rel="noopener noreferrer" {...anchorProps}>
       {children}
     </a>
   );
