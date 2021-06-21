5aa8726e2c18603bbac993b9ac59dac1f11f720a
谷 孝矩 <tani.takanori@tis.co.jp>
2021-06-21 18:20:28 +0900

    できない...

--- src/components/RepositoryLink.tsx
+++ src/components/RepositoryLink.tsx
@@ -4,18 +4,30 @@ import React, {ComponentProps, PropsWithChildren} from 'react';
 
 export type RepositoryLinkProps = {
   path?: string;
+  projectName?: string;
 };
 
 export const RepositoryLink: React.FC<PropsWithChildren<RepositoryLinkProps & ComponentProps<'a'>>> = ({
   path,
+  projectName,
   children,
   ...anchorProps
 }) => {
   const {siteConfig} = useDocusaurusContext() as DocusaurusContext;
-  const href = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}${path ?? ''}`;
+  const href = `https://github.com/${siteConfig.organizationName}/${
+    projectName ? projectName : siteConfig.projectName
+  }${path ?? ''}`;
   return (
     <a href={href} target="_blank" rel="noopener noreferrer" {...anchorProps}>
       {children}
     </a>
   );
+};
+
+export const GithubContentsUrl: React.FC<RepositoryLinkProps> = ({path, projectName}) => {
+  const {siteConfig} = useDocusaurusContext() as DocusaurusContext;
+  const url = `https://raw.githubusercontent.com/${siteConfig.organizationName}/${
+    projectName ? projectName : siteConfig.projectName
+  }${path ?? ''}`;
+  return <span>`${url}`</span>;
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './PageList';
+export * from './RepositoryLink';
