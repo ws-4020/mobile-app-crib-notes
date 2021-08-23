6a2e8068eb79518856b7a6f34b01cc254c4f40a3
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-08-23 18:00:37 +0900

    HTTP APIライブラリの選定は002にしました

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -237,7 +237,7 @@ module.exports = {
         },
         {
           type: 'doc',
-          id: 'react-native/santoku/decisions/adr-003-http-api-libraries',
+          id: 'react-native/santoku/decisions/adr-002-http-api-libraries',
         },
       ],
     },
