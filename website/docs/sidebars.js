334beaa2e31748524770bae1cb8f4fe2cf51aea2
谷 孝矩 <tani.takanori@tis.co.jp>
2021-09-02 16:41:43 +0900

    指摘対応

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -248,7 +248,7 @@ module.exports = {
             'react-native/santoku/application-architecture/deep-link/overview',
             'react-native/santoku/application-architecture/deep-link/process-deep-link',
             'react-native/santoku/application-architecture/deep-link/create-deep-link',
-            'react-native/santoku/application-architecture/deep-link/ios-patch',
+            'react-native/santoku/application-architecture/deep-link/react-native-firebase',
           ],
         },
       ],
