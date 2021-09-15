0f7a543c2f8e59f6f1d78fc7f402d67dcce70b75
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-09-16 03:15:18 +0900

    ✨: firebase.crashlyticsのモックもグローバルから参照できるように修正

---
+++ example-app/SantokuApp/jest/__mocks__/@react-native-firebase/crashlytics.ts
@@ -0,0 +1,27 @@
+import crashlytics from '@react-native-firebase/crashlytics';
+
+import mockedFirebase from './app';
+
+const mock: Omit<
+  ReturnType<typeof crashlytics>,
+  // nativeとemitterはprivateなのでここでは除外しておく。
+  'native' | 'emitter'
+> = {
+  app: mockedFirebase.app(),
+  checkForUnsentReports: jest.fn(),
+  crash: jest.fn(),
+  deleteUnsentReports: jest.fn(),
+  didCrashOnPreviousExecution: jest.fn(),
+  isCrashlyticsCollectionEnabled: true, // なぜか型定義上`true`しか許容されない、、、
+  log: jest.fn(),
+  sendUnsentReports: jest.fn(),
+  setAttribute: jest.fn(),
+  setAttributes: jest.fn(),
+  setCrashlyticsCollectionEnabled: jest.fn(),
+  setUserId: jest.fn(),
+  recordError: jest.fn(),
+};
+
+Object.defineProperty(__mocks, 'crashlytics', {value: mock});
+
+export default () => mock;
--- example-app/SantokuApp/jest/types/global.d.ts
+++ example-app/SantokuApp/jest/types/global.d.ts
@@ -1,8 +1,10 @@
-import {NavigationProp, ParamListBase} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 declare global {
   import Mocked = jest.Mocked;
   const __mocks: {
-    navigation: Mocked<NavigationProp<ParamListBase>>;
+    navigation: Mocked<ReturnType<typeof useNavigation>>;
+    crashlytics: Mocked<ReturnType<typeof crashlytics>>;
   };
 }
