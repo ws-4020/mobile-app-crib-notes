0d561acea67725a5b87a43fde51e3359c930780e
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-09-28 13:06:09 +0900

    不要なモックを削除

--- example-app/SantokuApp/src/framework/message/SystemMessage.test.ts
+++ example-app/SantokuApp/src/framework/message/SystemMessage.test.ts
@@ -1,24 +1,17 @@
-import {SystemErrorCode} from 'generated/BundledSystemMessages';
-
-import {loadSystemMessages, m, SystemMessagesLoader} from './SystemMessage';
+import {loadSystemMessages, m} from './SystemMessage';
 
 describe('SystemMessage systemMessage', () => {
   test('メッセージがロードされていない場合の検証', () => {
     expect(() => m('errors.webview.networkError')).toThrowError(new Error('System messages was not cached.'));
   });
   test('メッセージが取得できることの検証', () => {
-    const mockLoader = jest
-      .fn<SystemMessagesLoader<{[key in SystemErrorCode]: string}>, []>()
-      .mockImplementation(() => {
+    loadSystemMessages({
+      load: () => {
         return {
-          load: () => {
-            return {
-              'errors.webview.networkError': 'error message.',
-            };
-          },
+          'errors.webview.networkError': 'error message.',
         };
-      });
-    loadSystemMessages(mockLoader());
+      },
+    });
     expect(m('errors.webview.networkError')).toEqual('error message.');
   });
 });
