2f966d8fd7005dc9a037e2ac55757dfd2f8379db
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-10-08 17:58:36 +0900

    chore

--- example-app/SantokuApp/src/framework/firebase/FirebaseConfig.test.ts
+++ example-app/SantokuApp/src/framework/firebase/FirebaseConfig.test.ts
@@ -2,46 +2,34 @@ import firebase from '@react-native-firebase/app';
 
 import {firebaseConfig} from './FirebaseConfig';
 
-const mockAppWithNonDummySettings = {
-  name: 'mock',
-  options: {
-    appId: 'mocked firebase app id',
-    projectId: 'mocked firebase project id',
-  },
-  delete: jest.fn(),
-  utils: jest.fn(),
-  crashlytics: jest.fn(),
-};
-
-const mockAppWithDummySettings = {
-  name: 'dummy',
-  options: {
-    appId: 'dummy firebase app id',
-    projectId: 'santoku-app-dummy',
-  },
-  delete: jest.fn(),
-  utils: jest.fn(),
-  crashlytics: jest.fn(),
-};
-
 describe('Firebase Config', () => {
-  const spy = jest.spyOn(firebase, 'app');
-  afterEach(() => {
-    spy.mockClear();
-  });
+  // afterEach(() => {
+  //   spy.mockClear();
+  // });
   test('FirebaseインスタンスのOption情報を取得できるかの検証', () => {
-    spy.mockReturnValue(mockAppWithNonDummySettings);
+    // spy.mockReturnValue(mockAppWithNonDummySettings);
     expect(firebaseConfig.options).toEqual({
       appId: 'mocked firebase app id',
       projectId: 'mocked firebase project id',
     });
   });
   test('Firebaseの接続情報がダミーの場合の検証', () => {
-    spy.mockReturnValue(mockAppWithDummySettings);
+    const spy = jest.spyOn(firebase, 'app');
+    spy.mockReturnValue({
+      name: 'dummy',
+      options: {
+        appId: 'dummy firebase app id',
+        projectId: 'santoku-app-dummy',
+      },
+      delete: jest.fn(),
+      utils: jest.fn(),
+      crashlytics: jest.fn(),
+    });
     expect(firebaseConfig.isDummy).toEqual(true);
+    spy.mockRestore();
   });
   test('Firebaseの接続情報がダミーではない場合の検証', () => {
-    spy.mockReturnValue(mockAppWithNonDummySettings);
+    // spy.mockReturnValue(mockAppWithNonDummySettings);
     expect(firebaseConfig.isDummy).toEqual(false);
   });
 });
