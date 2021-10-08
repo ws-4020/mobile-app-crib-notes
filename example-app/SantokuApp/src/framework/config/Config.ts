784418fb86956080c02e784774a84cdfc498e114
kozake <sh.kozake@gmail.com>
2021-10-08 17:23:01 +0900

    TSDocの追加

--- example-app/SantokuApp/src/framework/config/Config.ts
+++ example-app/SantokuApp/src/framework/config/Config.ts
@@ -1,14 +1,32 @@
 import {Config as RNConfig} from 'react-native-config';
 
+/**
+ * 環境設定値を取得できます。
+ *
+ * ```ts
+ * import {config} from 'framework/config';
+ *
+ * console.log(Config.appNameHome);
+ * ```
+ */
 class Config {
+  /**
+   * `APP_ID_FLAVOR_SUFFIX`の設定値を返します。
+   */
   get appIdFlavorSuffix(): string {
     return RNConfig.APP_ID_FLAVOR_SUFFIX;
   }
 
+  /**
+   * `APP_NAME_HOME`の設定値を返します。
+   */
   get appNameHome(): string {
     return RNConfig.APP_NAME_HOME;
   }
 
+  /**
+   * `PROVISIONING_PROFILE_FLAVOR`の設定値を返します。
+   */
   get provisioningProfileFlavor(): string {
     return RNConfig.PROVISIONING_PROFILE_FLAVOR;
   }
