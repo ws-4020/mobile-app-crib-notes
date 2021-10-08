967a542c6923ad55fb7bdcb3777f4270963c695a
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-10-08 18:34:49 +0900

    ✅: とりあえず現時点のコードでテストが通るように戻す

--- example-app/SantokuApp/src/framework/config/Config.test.ts
+++ example-app/SantokuApp/src/framework/config/Config.test.ts
@@ -1,16 +1,16 @@
-import {Config} from './Config';
+import {config} from './Config';
 
 describe('Config', function () {
   it('react-native-configの値が取得されること', () => {
-    expect(Config.appIdFlavorSuffix).toBe('.test');
-    expect(Config.appNameHome).toBe('Test SantokuApp');
-    expect(Config.provisioningProfileFlavor).toBe('Not Available');
+    expect(config.appIdFlavorSuffix).toBe('.test');
+    expect(config.appNameHome).toBe('Test SantokuApp');
+    expect(config.provisioningProfileFlavor).toBe('Not Available');
   });
 
   it('jest.spyOnでモックの値を上書きできること', () => {
-    jest.spyOn(Config, 'appNameHome', 'get').mockReturnValue('string');
-    expect(Config.appIdFlavorSuffix).toBe('.test');
-    expect(Config.appNameHome).toBe('string');
-    expect(Config.provisioningProfileFlavor).toBe('Not Available');
+    jest.spyOn(config, 'appNameHome', 'get').mockReturnValue('string');
+    expect(config.appIdFlavorSuffix).toBe('.test');
+    expect(config.appNameHome).toBe('string');
+    expect(config.provisioningProfileFlavor).toBe('Not Available');
   });
 });
