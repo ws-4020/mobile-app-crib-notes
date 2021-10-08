9584f3ca57d6b0a35a757903cc96f466c4854638
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-10-08 12:01:43 +0900

    ✨: Expoに関する情報をApp Infoに追加

--- example-app/SantokuApp/src/screens/demo/info/useAppInfo.tsx
+++ example-app/SantokuApp/src/screens/demo/info/useAppInfo.tsx
@@ -1,6 +1,9 @@
 import * as Application from 'expo-application';
 import {firebase} from 'framework/native';
 import {Platform} from 'react-native';
+import {Constants} from 'react-native-unimodules';
+
+import {isExpoGo} from '../../../framework/utilities/environment';
 
 const info = [
   {
@@ -34,6 +37,26 @@ const info = [
   {
     name: 'Firebase App ID',
     value: firebase.app().options.appId,
+  },
+  {
+    name: 'Expo Go',
+    value: isExpoGo ? 'true' : 'false',
+  },
+  {
+    name: 'Expo Version',
+    value: Constants.expoVersion,
+  },
+  {
+    name: 'Expo Runtime Version',
+    value: Constants.expoRuntimeVersion,
+  },
+  {
+    name: 'Expo Execution Environment',
+    value: Constants.executionEnvironment,
+  },
+  {
+    name: 'Expo App Ownership',
+    value: Constants.appOwnership,
   },
 ];
 
