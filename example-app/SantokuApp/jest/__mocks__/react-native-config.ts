3ec16e722fc87ea54e7bef55c36292a1ca7666cb
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-10-08 18:44:22 +0900

    🤡: react-native-configのモックにspyOnできるように修正

--- example-app/SantokuApp/jest/__mocks__/react-native-config.ts
+++ example-app/SantokuApp/jest/__mocks__/react-native-config.ts
@@ -1,5 +1,11 @@
-export const Config = {
-  APP_ID_FLAVOR_SUFFIX: '.test',
-  APP_NAME_HOME: 'Test SantokuApp',
-  PROVISIONING_PROFILE_FLAVOR: 'Not Available',
-};
+export abstract class Config {
+  static get APP_ID_FLAVOR_SUFFIX() {
+    return '.test';
+  }
+  static get APP_NAME_HOME() {
+    return 'Test SantokuApp';
+  }
+  static get PROVISIONING_PROFILE_FLAVOR() {
+    return 'Not Available';
+  }
+}
