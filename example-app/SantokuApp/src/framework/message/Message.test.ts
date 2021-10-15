d8913818aab2a75ea40cba4791bacfe4056da8a7
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-10-15 11:46:08 +0900

    ts-ignoreの理由を記載

--- example-app/SantokuApp/src/framework/message/Message.test.ts
+++ example-app/SantokuApp/src/framework/message/Message.test.ts
@@ -3,46 +3,46 @@ import {loadMessages, m} from './Message';
 
 describe('Message message', () => {
   test('メッセージがロードされていない場合の検証', () => {
-    // @ts-ignore
+    // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
     expect(() => m('msg.error.network')).toThrowError(new Error('Messages was not cached.'));
   });
   test('オプションが存在しないメッセージの取得を検証', async () => {
     await loadMessages({
       load: async () => {
         return new Promise((resolve) => {
-          // @ts-ignore
+          // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
           resolve({'msg.error.network': 'network error.'});
         });
       },
     });
-    // @ts-ignore
+    // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
     expect(m('msg.error.network')).toEqual('network error.');
   });
   test('オプションが存在するメッセージの取得を検証', async () => {
     await loadMessages({
       load: async () => {
         return new Promise((resolve) => {
-          // @ts-ignore
+          // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
           resolve({'validation.required': '{0}を入力してください。'});
         });
       },
     });
-    // @ts-ignore
+    // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
     expect(m('validation.required', 'name')).toEqual('nameを入力してください。');
-    // @ts-ignore
+    // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
     expect(m('validation.required')).toEqual('{0}を入力してください。');
   });
   test('指定されたメッセージキーに該当するメッセージが存在しない場合の検証', async () => {
     await loadMessages({
       load: async () => {
         return new Promise((resolve) => {
-          // @ts-ignore
+          // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
           resolve({'msg.error.network': 'network error.'});
         });
       },
     });
     const spy = jest.spyOn(log, 'error');
-    // @ts-ignore
+    // @ts-ignore テスト用にMessageKeyには存在しないキーを指定しているため
     expect(m('dummyKey')).toEqual('dummyKey');
     expect(spy).toHaveBeenCalledWith('Message was not found. messageKey=[dummyKey]', 'MessageNotFound');
   });
