36d082c27a6405cfccdf050b84b6331be6c16ec6
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-10-25 10:54:31 +0900

    デフォルトのタイムゾーンを指定したので、dayjsのタイムゾーン設定は不要

--- example-app/SantokuApp/jest.config.js
+++ example-app/SantokuApp/jest.config.js
@@ -3,9 +3,5 @@ module.exports = {
   preset: 'jest-expo',
   // __mocks__ディレクトリをjestディレクトリ内に配置したいので、ルートディレクトリにjestを設定しています。
   roots: ['<rootDir>/src', 'jest'],
-  setupFiles: [
-    '<rootDir>/jest/setup/global.js',
-    '<rootDir>/jest/setup/react-navigation.js',
-    '<rootDir>/jest/setup/dayjs.js',
-  ],
+  setupFiles: ['<rootDir>/jest/setup/global.js', '<rootDir>/jest/setup/react-navigation.js'],
 };
--- example-app/SantokuApp/jest/setup/dayjs.js
+++
@@ -1,7 +0,0 @@
-const dayjs = require('dayjs');
-const timezone = require('dayjs/plugin/timezone');
-const utc = require('dayjs/plugin/utc');
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-dayjs.tz.setDefault('Asia/Tokyo');
