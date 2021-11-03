b28f428e14c8657a7fe298b55877d3a1ae2e98b9
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-11-03 18:41:26 +0900

    ⬆: Upgrade React Native 0.63.4 -> 0.64.2
    https://react-native-community.github.io/upgrade-helper/?from=0.63.4&to=0.64.2

--- example-app/SantokuApp/.prettierrc.js
+++ example-app/SantokuApp/.prettierrc.js
@@ -6,10 +6,16 @@ module.exports = {
   // https://prettier.io/docs/en/options.html#bracket-line
   // JSXのタグが複数行になったときに、最後の'>'の前で改行しない。
   bracketSameLine: true,
+  // https://prettier.io/docs/en/options.html#deprecated-jsx-brackets
+  // JSXのタグが複数行になったときに、最後の'>'の前で改行しない。
+  jsxBracketSameLine: true,
   // https://prettier.io/docs/en/options.html#quotes
   // ダブルクオートではなく、シングルクオートに統一する。
   singleQuote: true,
   // https://prettier.io/docs/en/options.html#trailing-commas
   // 配列などの最後の要素の後にもカンマを付けるようにする。（順番を入れ替えたりするときに行を入れ替えるだけで良くなるので便利です。）
   trailingComma: 'all',
+  // https://prettier.io/docs/en/options.html#arrow-function-parentheses
+  // アロー関数の引数に不要なカッコを付けないようにする。
+  arrowParens: 'avoid',
 };
