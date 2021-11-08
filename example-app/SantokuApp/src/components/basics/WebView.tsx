99aa4d05d3973271b1dc9105fd1f740c88eb386e
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-08 16:25:32 +0900

    リロードのハンドリング

--- example-app/SantokuApp/src/components/basics/WebView.tsx
+++ example-app/SantokuApp/src/components/basics/WebView.tsx
@@ -1,15 +1,32 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useImperativeHandle, useRef} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 import {WebView as RNWebView} from 'react-native-webview';
-import {WebViewScrollEvent} from 'react-native-webview/lib/WebViewTypes';
+import {WebViewErrorEvent, WebViewHttpErrorEvent, WebViewScrollEvent} from 'react-native-webview/lib/WebViewTypes';
+
+export type WebViewHandler = {
+  reload: () => void;
+};
 
 type Props = {
   uri: string;
   onScrollEnd: () => void;
+  onError?: (event: WebViewErrorEvent) => void;
+  onHttpError?: (event: WebViewHttpErrorEvent) => void;
 };
 
-export const WebView: React.FC<Props> = ({uri, onScrollEnd}) => {
+export const WebView = React.forwardRef<WebViewHandler, Props>(function WebView(
+  {uri, onScrollEnd, onError, onHttpError},
+  ref,
+) {
+  const webViewRef = useRef<RNWebView>(null);
+
+  useImperativeHandle(ref, () => ({
+    reload: () => {
+      webViewRef.current?.reload();
+    },
+  }));
+
   const handleScroll = useCallback(
     (event: WebViewScrollEvent) => {
       const scrollY = event.nativeEvent.contentOffset.y + event.nativeEvent.layoutMeasurement.height;
@@ -22,15 +39,18 @@ export const WebView: React.FC<Props> = ({uri, onScrollEnd}) => {
 
   return (
     <RNWebView
+      // androidLayerType：WebViewを再レンダー時にクラッシュする可能性がある問題の回避策
+      // https://github.com/react-native-webview/react-native-webview/issues/1915
       androidLayerType="software"
       style={styles.container}
       source={{uri}}
       onScroll={handleScroll}
-      onError={(error) => console.log('onError', error)}
-      onHttpError={(error) => console.log('onHttpError', error)}
+      onError={onError}
+      onHttpError={onHttpError}
+      ref={webViewRef}
     />
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
