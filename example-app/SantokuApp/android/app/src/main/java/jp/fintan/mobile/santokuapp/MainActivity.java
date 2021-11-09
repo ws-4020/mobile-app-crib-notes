016fc99e310676fb549500b1497567cc06d81e11
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-09 16:01:25 +0900

    コメント付与

--- example-app/SantokuApp/android/app/src/main/java/jp/fintan/mobile/santokuapp/MainActivity.java
+++ example-app/SantokuApp/android/app/src/main/java/jp/fintan/mobile/santokuapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+// Transition中にActivityのbackgroundが表示される問題があるため、Splash Screen用とReact Native用にActivityを分ける.
+// https://github.com/software-mansion/react-native-screens/issues/380
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
