dd5230cfbfa8585d86265a1cddd2c878aad2db17
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-15 15:07:24 +0900

    スナックバーの共通処理

--- example-app/SantokuApp/src/components/snackbar/WithSnackbar.tsx
+++ example-app/SantokuApp/src/components/snackbar/WithSnackbar.tsx
@@ -1,21 +1,34 @@
+import {m} from 'framework';
 import React, {Dispatch, SetStateAction, useContext, useMemo, useState} from 'react';
 
 import {Snackbar, SnackbarHideProps, SnackbarProps, SnackbarShowProps} from './Snackbar';
 
 type SnackbarHideContextProps = Omit<SnackbarHideProps, 'hide'>;
+type SnackbarShowCloseButtonContextProps = Omit<SnackbarShowProps, 'actionText' | 'actionHandler'>;
 
 type SnackbarContextType = {
   /**
    * Show snackbar.
    */
   show: Dispatch<SetStateAction<SnackbarShowProps>>;
+
+  /**
+   * Show the snack bar with the close button.
+   * The close button is placed to the right of the message.
+   */
+  showWithCloseButton: (showProps: SnackbarShowCloseButtonContextProps) => void;
+
   /**
    * Hide snackbar.
    */
   hide: (hideProps?: SnackbarHideContextProps) => void;
 };
 
-const SnackbarContext = React.createContext<SnackbarContextType>({show: () => {}, hide: () => {}});
+const SnackbarContext = React.createContext<SnackbarContextType>({
+  show: () => {},
+  showWithCloseButton: () => {},
+  hide: () => {},
+});
 
 export function useSnackbar() {
   return useContext(SnackbarContext);
@@ -30,6 +43,14 @@ export function WithSnackbar(props: {initialState?: SnackbarShowProps; children:
   const snackbarContext = useMemo(
     () => ({
       show: setState,
+      showWithCloseButton: (showProps: SnackbarShowCloseButtonContextProps) => {
+        setState((prevState) => ({
+          ...prevState,
+          ...showProps,
+          actionText: m('閉じる'),
+          actionHandler: snackbarContext.hide,
+        }));
+      },
       hide: (hideProps?: SnackbarHideContextProps) => {
         setState((prevState) => ({...prevState, ...hideProps, hide: true}));
       },
