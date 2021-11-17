3f2ffe1d582ccb437cc3366da3d40865de899a2e
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-11-17 13:37:51 +0900

    expo-secure-storeを追加

--- example-app/SantokuApp/src/framework/authn/AuthnService.ts
+++ example-app/SantokuApp/src/framework/authn/AuthnService.ts
@@ -1,23 +1,43 @@
 import {Account, AccountLoginResponse} from '../../generated/api';
 import {api} from '../backend';
+import {SecureStorageAdapter} from './SecureStorageAdapter';
 
+class AccountIdNotFoundError extends Error {}
+class PasswordNotFoundError extends Error {}
 async function signup(nickname: string, password: string): Promise<Account> {
   const res = await api.postSignup({nickname, password});
-  // TODO セキュアストレージに格納
+  const accountId = res.data.accountId;
+  await Promise.all([
+    SecureStorageAdapter.saveActiveAccountId(accountId),
+    SecureStorageAdapter.savePassword(accountId, password),
+  ]);
   return res.data;
 }
 
 async function login(accountId: string): Promise<AccountLoginResponse> {
-  // TODO セキュアストレージから取得
-  const password = '';
+  const password = await SecureStorageAdapter.loadPassword(accountId);
+  if (!password) {
+    throw new PasswordNotFoundError('The password for the account ID does not exist.');
+  }
   const res = await api.postLogin({accountId, password});
   return res.data;
 }
 
 async function autoLogin(): Promise<AccountLoginResponse> {
-  // TODO セキュアストレージから取得
-  const accountId = '';
+  const accountId = await SecureStorageAdapter.loadActiveAccountId();
+  if (!accountId) {
+    throw new AccountIdNotFoundError('There is no auto-login enabled account.');
+  }
   return login(accountId);
+}
+
+async function canAutoLogin(): Promise<boolean> {
+  const accountId = await SecureStorageAdapter.loadActiveAccountId();
+  if (!accountId) {
+    return false;
+  }
+  const password = await SecureStorageAdapter.loadPassword(accountId);
+  return !!password;
 }
 
 async function refresh(): Promise<AccountLoginResponse> {
@@ -31,6 +51,8 @@ async function logout(): Promise<void> {
 export const AuthnService = {
   signup,
   login,
+  canAutoLogin,
+  autoLogin,
   refresh,
   logout,
 };
---
+++ example-app/SantokuApp/src/framework/authn/SecureStorageAdapter.ts
@@ -0,0 +1,36 @@
+import * as SecureStore from 'expo-secure-store';
+
+const STORED_ITEM_KEYS = {
+  ACTIVE_ACCOUNT_ID: 'activeAccountId',
+  PASSWORD: 'password',
+} as const;
+
+function saveActiveAccountId(accountId: string): Promise<void> {
+  return SecureStore.setItemAsync(STORED_ITEM_KEYS.ACTIVE_ACCOUNT_ID, accountId, {
+    keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+  });
+}
+
+function savePassword(accountId: string, password: string): Promise<void> {
+  return SecureStore.setItemAsync(STORED_ITEM_KEYS.PASSWORD, password, {
+    keychainAccessible: SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+    keychainService: accountId,
+  });
+}
+
+function loadActiveAccountId(): Promise<string | null> {
+  return SecureStore.getItemAsync(STORED_ITEM_KEYS.ACTIVE_ACCOUNT_ID);
+}
+
+function loadPassword(accountId: string): Promise<string | null> {
+  return SecureStore.getItemAsync(STORED_ITEM_KEYS.PASSWORD, {
+    keychainService: accountId,
+  });
+}
+
+export const SecureStorageAdapter = {
+  saveActiveAccountId,
+  savePassword,
+  loadActiveAccountId,
+  loadPassword,
+};
