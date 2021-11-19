c7104cdad20595e56163f257da103ba5bd085025
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-11-19 17:22:02 +0900

    ローカル認証のデモ画面をクラスコンポーネントではなく関数化

--- example-app/SantokuApp/src/screens/demo/local-authn/LocalAuthnScreen.tsx
+++ example-app/SantokuApp/src/screens/demo/local-authn/LocalAuthnScreen.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {Button} from 'react-native-elements';
 
-import LocalAuthnTestScreen from './LocalAuthnTestScreen';
+import localAuthentication from '../../../framework/local-authentication';
 
-const ScreenName = 'LocalAuthn';
+const ScreenName = 'Local Authentication';
 const Screen = () => {
+  const isEnrolled = async () => {
+    const isEnrolled: boolean = await localAuthentication.isEnrolled();
+    alert(isEnrolled);
+  };
+
+  const isFingerPrintSupported = async () => {
+    const isSupported = await localAuthentication.isFingerPrintSupported();
+    alert(isSupported);
+  };
+
+  const isFacialSupported = async () => {
+    const isSupported = await localAuthentication.isFacialSupported();
+    alert(isSupported);
+  };
+
+  const isIrisSupported = async () => {
+    const isSupported = await localAuthentication.isIrisSupported();
+    alert(isSupported);
+  };
+
+  const localAuthentication1 = async () => {
+    const result = await localAuthentication.authenticate({
+      promptMessage: 'Test Message',
+      disableFallback: true,
+      cancelLabel: 'キャンセル', // FallbackをOFFにしている場合は必須
+    });
+    alert(JSON.stringify(result));
+  };
+
+  const localAuthentication2 = async () => {
+    const result = await localAuthentication.authenticate({
+      promptMessage: '認証テスト',
+      cancelLabel: 'キャンセル',
+      fallbackLabel: '失敗時の基本認証',
+      disableFallback: false,
+    });
+    alert(JSON.stringify(result));
+  };
+
   return (
     <View style={styles.container}>
-      <LocalAuthnTestScreen />
+      <Button onPress={isEnrolled} title="デバイスの設定チェック" />
+      <Button onPress={isFingerPrintSupported} title="指紋認証のサポートのチェック" />
+      <Button onPress={isFacialSupported} title="顔認証のサポートのチェック" />
+      <Button onPress={isIrisSupported} title="虹彩認証のサポートのチェック" />
+      <Button onPress={localAuthentication1} title="認証の実行 Option指定3つ" />
+      <Button onPress={localAuthentication2} title="認証の実行 Option全指定" />
     </View>
   );
 };
