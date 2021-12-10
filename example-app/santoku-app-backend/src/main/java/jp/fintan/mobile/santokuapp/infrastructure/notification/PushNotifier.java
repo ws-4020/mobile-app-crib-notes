3a7b881ab0f920585c940fd31748b6f4a230963b
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-12-10 11:54:54 +0900

    multicastで送信するように変更。送信失敗時はExceptionをthrowしないで、続行するように変更

--- example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/infrastructure/notification/PushNotifier.java
+++ example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/infrastructure/notification/PushNotifier.java
@@ -1,10 +1,15 @@
 package jp.fintan.mobile.santokuapp.infrastructure.notification;
 
+import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MessagingErrorCode;
+import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.SendResponse;
 import java.util.List;
+import java.util.stream.Collectors;
+import jp.fintan.mobile.santokuapp.domain.model.core.ValueObject;
 import jp.fintan.mobile.santokuapp.domain.model.notification.PushNotification;
 import jp.fintan.mobile.santokuapp.domain.model.notification.PushNotificationToken;
 import jp.fintan.mobile.santokuapp.domain.repository.PushNotificationRepository;
@@ -19,29 +24,57 @@ public class PushNotifier implements PushNotificationRepository {
   @Override
   public void notifyToDevice(
       PushNotification pushNotification, List<PushNotificationToken> tokens) {
-    tokens.forEach(
-        token -> {
-          // Build message
-          Message.Builder messageBuilder = Message.builder();
-          messageBuilder.setToken(token.value());
 
-          Notification.Builder notificationBuilder = Notification.builder();
-          notificationBuilder.setTitle(pushNotification.title().value());
-          notificationBuilder.setBody(pushNotification.body().value());
-          Notification notification = notificationBuilder.build();
+    final List<String> fcmTokens =
+        tokens.stream().map(ValueObject::value).collect(Collectors.toList());
+    // Build message
+    MulticastMessage.Builder multicastMessageBuilder = MulticastMessage.builder();
+    multicastMessageBuilder.addAllTokens(fcmTokens);
 
-          messageBuilder.setNotification(notification);
-          messageBuilder.putData("type", pushNotification.type().value());
-          Message message = messageBuilder.build();
+    Notification.Builder notificationBuilder = Notification.builder();
+    notificationBuilder.setTitle(pushNotification.title().value());
+    notificationBuilder.setBody(pushNotification.body().value());
+    Notification notification = notificationBuilder.build();
 
-          // Send a message to the device corresponding to the provided token.
-          try {
-            // Response is a message ID string.
-            String response = FirebaseMessaging.getInstance().send(message);
-            LOGGER.logInfo("Successfully sent message: " + response);
-          } catch (FirebaseMessagingException e) {
-            throw new RuntimeException("Failed to send message to Firebase Cloud Messaging.", e);
-          }
-        });
+    multicastMessageBuilder.setNotification(notification);
+    multicastMessageBuilder.putData("type", pushNotification.type().value());
+    MulticastMessage multicastMessage = multicastMessageBuilder.build();
+
+    // Send a message to the device corresponding to the provided token.
+    try {
+      // Response is a message ID string.
+      BatchResponse batchResponse = FirebaseMessaging.getInstance().sendMulticast(multicastMessage);
+      final List<SendResponse> responses = batchResponse.getResponses();
+      for (int i = 0; i < responses.size(); i++) {
+        final SendResponse response = responses.get(i);
+        final String fcmToken = fcmTokens.get(i);
+        if (response.isSuccessful()) {
+          LOGGER.logInfo(String.format("Successfully sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken));
+          return;
+        }
+        error(response, fcmToken);
+      }
+    } catch (FirebaseMessagingException e) {
+      LOGGER.logError("Failed to sent message to Firebase Cloud Messaging.", e);
+    }
+  }
+
+  private void error(SendResponse response, String fcmToken) {
+    final FirebaseMessagingException e = response.getException();
+    final MessagingErrorCode errorCode = e.getMessagingErrorCode();
+    switch (errorCode) {
+      case THIRD_PARTY_AUTH_ERROR:
+      case INTERNAL:
+      case QUOTA_EXCEEDED:
+      case SENDER_ID_MISMATCH:
+      case UNAVAILABLE:
+        LOGGER.logError(String.format("Failed to sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken), e);
+        break;
+      case INVALID_ARGUMENT:
+      case UNREGISTERED:
+        LOGGER.logWarn(String.format("Failed to sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken), e);
+        // TODO: 無効なトークンの削除処理
+        break;
+    }
   }
 }
