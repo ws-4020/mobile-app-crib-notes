333b2700b08bb0b14b09bf7884935a18136585bc
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-12-13 18:00:22 +0900

    通知設定関連を変更

--- example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/infrastructure/notification/PushNotifier.java
+++ example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/infrastructure/notification/PushNotifier.java
@@ -2,6 +2,8 @@ package jp.fintan.mobile.santokuapp.infrastructure.notification;
 
 import com.google.firebase.messaging.AndroidConfig;
 import com.google.firebase.messaging.AndroidConfig.Priority;
+import com.google.firebase.messaging.ApnsConfig;
+import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
@@ -9,7 +11,9 @@ import com.google.firebase.messaging.MessagingErrorCode;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.SendResponse;
+import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import jp.fintan.mobile.santokuapp.domain.model.core.ValueObject;
 import jp.fintan.mobile.santokuapp.domain.model.notification.PushNotification;
@@ -22,6 +26,10 @@ import nablarch.core.repository.di.config.externalize.annotation.SystemRepositor
 @SystemRepositoryComponent
 public class PushNotifier implements PushNotificationRepository {
   private static final Logger LOGGER = LoggerManager.get(PushNotifier.class);
+  // TTLは12時間に設定
+  private static final long TTL = 43200;
+  // APNSの通知優先度
+  private static final String APNS_PRIORITY = "10";
 
   @Override
   public void notifyToDevice(
@@ -32,21 +40,10 @@ public class PushNotifier implements PushNotificationRepository {
     // Build message
     MulticastMessage.Builder multicastMessageBuilder = MulticastMessage.builder();
     multicastMessageBuilder.addAllTokens(fcmTokens);
-
-    Notification.Builder notificationBuilder = Notification.builder();
-    notificationBuilder.setTitle(pushNotification.title().value());
-    notificationBuilder.setBody(pushNotification.body().value());
-    notificationBuilder.setImage("https://foo.bar.pizza-monster.png");
-    Notification notification = notificationBuilder.build();
-
-    AndroidConfig.Builder androidConfigBuilder = AndroidConfig.builder();
-    androidConfigBuilder.setPriority(Priority.HIGH);
-    androidConfigBuilder.setCollapseKey(pushNotification.type().name());
-    AndroidConfig androidConfig = androidConfigBuilder.build();
-
-    multicastMessageBuilder.setNotification(notification);
     multicastMessageBuilder.putData("type", pushNotification.type().name());
-    multicastMessageBuilder.setAndroidConfig(androidConfig);
+    multicastMessageBuilder.setNotification(createNotification(pushNotification));
+    multicastMessageBuilder.setApnsConfig(createApnsConfig());
+    multicastMessageBuilder.setAndroidConfig(createAndroidConfig(pushNotification));
     MulticastMessage multicastMessage = multicastMessageBuilder.build();
 
     // Send a message to the device corresponding to the provided token.
@@ -58,7 +55,10 @@ public class PushNotifier implements PushNotificationRepository {
         final SendResponse response = responses.get(i);
         final String fcmToken = fcmTokens.get(i);
         if (response.isSuccessful()) {
-          LOGGER.logInfo(String.format("Successfully sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken));
+          LOGGER.logInfo(
+              String.format(
+                  "Successfully sent message to Firebase Cloud Messaging.. fcmToken=[%s]",
+                  fcmToken));
           return;
         }
         error(response, fcmToken);
@@ -66,6 +66,35 @@ public class PushNotifier implements PushNotificationRepository {
     } catch (FirebaseMessagingException e) {
       LOGGER.logError("Failed to sent message to Firebase Cloud Messaging.", e);
     }
+  }
+
+  private Notification createNotification(PushNotification pushNotification) {
+    Notification.Builder notificationBuilder = Notification.builder();
+    notificationBuilder.setTitle(pushNotification.title().value());
+    notificationBuilder.setBody(pushNotification.body().value());
+    return notificationBuilder.build();
+  }
+
+  private ApnsConfig createApnsConfig() {
+    // APNSは有効期限をエポック秒で指定する必要がある
+    final String apnsExpiration = Long.toString(Instant.now().plusSeconds(TTL).getEpochSecond());
+    // 指定するキーは以下を参照
+    // https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns?language=objc
+    ApnsConfig.Builder apnsConfigBuilder = ApnsConfig.builder();
+    apnsConfigBuilder.putAllHeaders(
+        Map.of("apns-priority", APNS_PRIORITY, "apns-expiration", apnsExpiration));
+    Aps aps =Aps.builder().build();
+    apnsConfigBuilder.setAps(aps);
+    return apnsConfigBuilder.build();
+  }
+
+  private AndroidConfig createAndroidConfig(PushNotification pushNotification) {
+
+    AndroidConfig.Builder androidConfigBuilder = AndroidConfig.builder();
+    androidConfigBuilder.setPriority(Priority.HIGH);
+    androidConfigBuilder.setCollapseKey(pushNotification.type().name());
+    androidConfigBuilder.setTtl(TTL);
+    return androidConfigBuilder.build();
   }
 
   private void error(SendResponse response, String fcmToken) {
@@ -77,11 +106,17 @@ public class PushNotifier implements PushNotificationRepository {
       case QUOTA_EXCEEDED:
       case SENDER_ID_MISMATCH:
       case UNAVAILABLE:
-        LOGGER.logError(String.format("Failed to sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken), e);
+        LOGGER.logError(
+            String.format(
+                "Failed to sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken),
+            e);
         break;
       case INVALID_ARGUMENT:
       case UNREGISTERED:
-        LOGGER.logWarn(String.format("Failed to sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken), e);
+        LOGGER.logWarn(
+            String.format(
+                "Failed to sent message to Firebase Cloud Messaging.. fcmToken=[%s]", fcmToken),
+            e);
         // TODO: 無効なトークンの削除処理
         break;
     }
