5f725a475faef61283b49340d61a1cbc5959a20c
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2021-12-15 10:50:50 +0900

    チームAPIに関する部品は別ブランチに移動

--- example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/domain/repository/TimetableRepository.java
+++
@@ -1,9 +0,0 @@
-package jp.fintan.mobile.santokuapp.domain.repository;
-
-import jp.fintan.mobile.santokuapp.domain.model.team.TeamId;
-import jp.fintan.mobile.santokuapp.domain.model.timetable.Timetable;
-import jp.fintan.mobile.santokuapp.domain.model.timetable.TimetableId;
-
-public interface TimetableRepository {
-  Timetable find(TeamId teamId, TimetableId timetableId);
-}
