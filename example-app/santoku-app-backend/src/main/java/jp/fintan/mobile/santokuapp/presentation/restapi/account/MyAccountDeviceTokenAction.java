744c4ed5697747d00223ceebdbb85dbd454453c0
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2022-02-03 09:40:21 +0900

    不要なimportを削除

--- example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/presentation/restapi/account/MyAccountDeviceTokenAction.java
+++ example-app/santoku-app-backend/src/main/java/jp/fintan/mobile/santokuapp/presentation/restapi/account/MyAccountDeviceTokenAction.java
@@ -1,11 +1,8 @@
 package jp.fintan.mobile.santokuapp.presentation.restapi.account;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import jp.fintan.mobile.santokuapp.application.service.LoginAccountIdSupplier;
 import jp.fintan.mobile.santokuapp.application.service.account.AccountDeviceTokenService;
