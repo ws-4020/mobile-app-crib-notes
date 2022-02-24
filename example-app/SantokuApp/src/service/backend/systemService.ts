580808865058e258770eef44c6070e310f2d7bd8
kozake <sh.kozake@gmail.com>
2022-02-24 19:44:40 +0900

    useGetCsrfTokenはimport/exportするだけに変更

--- example-app/SantokuApp/src/service/backend/systemService.ts
+++ example-app/SantokuApp/src/service/backend/systemService.ts
@@ -1,15 +1,3 @@
-import {setCsrfTokenHeader} from 'framework/backend/customInstance';
-import {useGetCsrfToken as useGetCsrfTokenApi} from 'generated/backend/system/system';
-
-const useGetCsrfToken = (options?: {enabled: boolean}) => {
-  return useGetCsrfTokenApi({
-    query: {
-      enabled: options?.enabled,
-      onSuccess: response => {
-        setCsrfTokenHeader(response.data.csrfTokenHeaderName, response.data.csrfTokenValue);
-      },
-    },
-  });
-};
+import {useGetCsrfToken} from 'generated/backend/system/system';
 
 export {useGetCsrfToken};
