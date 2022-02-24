c9c8d3cc68ed23c612683fac1922812f85ca91da
kozake <sh.kozake@gmail.com>
2022-02-24 19:46:04 +0900

    fix comment

--- example-app/SantokuApp/src/screens/demo/react-query/useDependentQueryDemo.ts
+++ example-app/SantokuApp/src/screens/demo/react-query/useDependentQueryDemo.ts
@@ -22,7 +22,7 @@ const useDependentQueryDemo = () => {
   // 依存関係のあるAPI呼び出しは処理を1つのQuery Functionにまとめた上でuseQueryを用いる
   const getTodoDetailsQuery = useGetTodoDetails(queryParameters, {enabled: queryEnabled});
 
-  // 並列クエリ (上記のクエリとGET /accounts/meを並列で呼び出し)
+  // 並列クエリ (上記のクエリとGET /csrf_tokenを並列で呼び出し)
   // 並列で実行したクエリの総合的な結果をもとに画面描画を切り替える場合はそれぞれのクエリのstatusを統合する
   const getCsrfTokenQuery = useGetCsrfToken();
   const {isIdle, isLoading, isSuccess, isError} = getCombinedQueryStatus([getTodoDetailsQuery, getCsrfTokenQuery]);
