1a6a28131eadcce61e704eb107b4d99b32da9d2e
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2022-03-31 20:17:56 +0900

    ♻: Change configuration key order

--- example-app/SantokuApp/.eslintrc.js
+++ example-app/SantokuApp/.eslintrc.js
@@ -8,18 +8,18 @@ module.exports = {
   extends: ['universe/native', 'plugin:react-hooks/recommended'],
   overrides: [
     {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
       // universe/shared/typescript-analysis: https://github.com/expo/expo/tree/master/packages/eslint-config-universe
       // recommended-requiring-type-checking: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
       extends: ['universe/shared/typescript-analysis', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
-      files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
         project: './tsconfig.json',
       },
     },
     {
+      files: ['*.test.ts', '*.test.tsx'],
       // https://github.com/jest-community/eslint-plugin-jest#shareable-configurations
       extends: ['plugin:jest/recommended'],
-      files: ['*.test.ts', '*.test.tsx'],
       plugins: ['jest'],
       rules: {
         // you should turn the original rule off *only* for test files
