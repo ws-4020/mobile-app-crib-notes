505493c5dd977c57358a5c48a1c9a2396eda273e
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2022-04-19 14:09:58 +0900

    PickerAccessoryの各テキストコンポーネントを指定できるように変更しました

--- example-app/SantokuApp/src/components/picker/PickerAccessory.tsx
+++ example-app/SantokuApp/src/components/picker/PickerAccessory.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StyleProp, StyleSheet, TouchableOpacity, View, ViewStyle} from 'react-native';
-import {Text, TextProps} from 'react-native-elements';
+import {StyleProp, StyleSheet, Text, TextProps, TouchableOpacity, View, ViewStyle} from 'react-native';
 
 export type PickerAccessoryProps = {
   onDelete?: () => void;
@@ -13,9 +12,10 @@ export type PickerAccessoryProps = {
   cancelLabel?: string;
   doneLabel?: string;
   pickerAccessoryStyle?: StyleProp<ViewStyle>;
+  deleteTextComponent?: React.ReactNode;
+  cancelTextComponent?: React.ReactNode;
+  doneTextComponent?: React.ReactNode;
 };
-
-// TODO: Textコンポーネントを指定できるようにする
 
 export const PickerAccessory: React.FC<PickerAccessoryProps> = ({
   onDelete,
@@ -28,31 +28,46 @@ export const PickerAccessory: React.FC<PickerAccessoryProps> = ({
   deleteLabel,
   cancelLabel,
   doneLabel,
+  deleteTextComponent,
+  cancelTextComponent,
+  doneTextComponent,
 }) => {
   return (
-    <View style={StyleSheet.flatten([styles.container, pickerAccessoryStyle])}>
+    <View style={StyleSheet.flatten([styles.container, pickerAccessoryStyle])} testID="PickerAccessory">
       {deleteLabel && (
         <>
-          <TouchableOpacity onPress={onDelete}>
-            <Text style={StyleSheet.flatten([styles.deleteTextStyle, deleteTextStyle])} {...deleteTextProps}>
-              {deleteLabel}
-            </Text>
+          <TouchableOpacity onPress={onDelete} testID="deleteLink">
+            {deleteTextComponent ? (
+              deleteTextComponent
+            ) : (
+              <Text style={StyleSheet.flatten([styles.deleteTextStyle, deleteTextStyle])} {...deleteTextProps}>
+                {deleteLabel}
+              </Text>
+            )}
           </TouchableOpacity>
           <View style={{flex: 1}} />
         </>
       )}
       {cancelLabel && (
-        <TouchableOpacity onPress={onCancel}>
-          <Text style={StyleSheet.flatten([styles.cancelTextStyle, cancelTextStyle])} {...cancelTextProps}>
-            {cancelLabel}
-          </Text>
+        <TouchableOpacity onPress={onCancel} testID="cancelLink">
+          {cancelTextComponent ? (
+            cancelTextComponent
+          ) : (
+            <Text style={StyleSheet.flatten([styles.cancelTextStyle, cancelTextStyle])} {...cancelTextProps}>
+              {cancelLabel}
+            </Text>
+          )}
         </TouchableOpacity>
       )}
       {doneLabel && (
-        <TouchableOpacity onPress={onDone}>
-          <Text style={StyleSheet.flatten([styles.doneTextStyle, doneTextStyle])} {...doneTextProps}>
-            {doneLabel}
-          </Text>
+        <TouchableOpacity onPress={onDone} testID="doneLink">
+          {doneTextComponent ? (
+            doneTextComponent
+          ) : (
+            <Text style={StyleSheet.flatten([styles.doneTextStyle, doneTextStyle])} {...doneTextProps}>
+              {doneLabel}
+            </Text>
+          )}
         </TouchableOpacity>
       )}
     </View>
