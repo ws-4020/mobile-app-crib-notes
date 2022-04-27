5eaada46f6a2b1c02cadbc6a207bedb4973aca0e
creature-water-valley <mizutani.wataru.569@tis.co.jp>
2022-04-27 17:55:54 +0900

    refactor

--- example-app/SantokuApp/src/components/picker/useYearMonthPickerAndroidUseCase.ts
+++ example-app/SantokuApp/src/components/picker/useYearMonthPickerAndroidUseCase.ts
@@ -1,5 +1,6 @@
 import React, {useCallback, useMemo} from 'react';
 
+import {Item} from './SelectPicker';
 import {YearMonthPickerProps} from './YearMonthPicker';
 import {useYearMonthPickerUseCase} from './useYearMonthPickerUseCase';
 
@@ -16,42 +17,47 @@ export const useYearMonthPickerAndroidUseCase = (props: YearMonthPickerProps) =>
     itemStyle,
   } = props;
 
+  const withUnselectItem = useCallback(
+    (items: Item<number>[]) => (unselectItem ? [unselectItem].concat(items) : items),
+    [unselectItem],
+  );
+
   const monthItems = useMemo(() => {
     const maximumYear = maximumYearMonth.year;
     const minimumYear = minimumYearMonth.year;
     const maximumMonth = maximumYearMonth.month;
     const minimumMonth = minimumYearMonth.month;
 
+    const itemProps = {color: itemColor, fontFamily: itemFontFamily, style: itemStyle};
     if (maximumYear === minimumYear) {
       const items = [...Array<number>(maximumMonth - minimumMonth + 1)].map((_, index: number) => {
         const m = minimumMonth + index;
-        return {value: m, label: String(m), color: itemColor, fontFamily: itemFontFamily, style: itemStyle};
+        return {value: m, label: String(m), ...itemProps};
       });
-      return unselectItem ? [unselectItem].concat(items) : items;
+      return withUnselectItem(items);
     }
 
     if (selectedYear === maximumYear) {
       const items = [...Array<number>(maximumMonth)].map((_, index: number) => {
         const m = index + 1;
-        return {value: m, label: String(m), color: itemColor, fontFamily: itemFontFamily, style: itemStyle};
+        return {value: m, label: String(m), ...itemProps};
       });
-      return unselectItem ? [unselectItem].concat(items) : items;
+      return withUnselectItem(items);
     }
 
     if (selectedYear === minimumYear) {
       const items = [...Array<number>(12 - minimumMonth + 1)].map((_, index: number) => {
         const m = minimumMonth + index;
-        return {value: m, label: String(m), color: itemColor, fontFamily: itemFontFamily, style: itemStyle};
+        return {value: m, label: String(m), ...itemProps};
       });
-      return unselectItem ? [unselectItem].concat(items) : items;
+      return withUnselectItem(items);
     }
 
     const items = [...Array<number>(12)].map((_, index: number) => {
       const m = index + 1;
-      return {value: m, label: String(m), color: itemColor, fontFamily: itemFontFamily, style: itemStyle};
+      return {value: m, label: String(m), ...itemProps};
     });
-
-    return unselectItem ? [unselectItem].concat(items) : items;
+    return withUnselectItem(items);
   }, [
     itemColor,
     itemFontFamily,
@@ -61,7 +67,7 @@ export const useYearMonthPickerAndroidUseCase = (props: YearMonthPickerProps) =>
     minimumYearMonth.month,
     minimumYearMonth.year,
     selectedYear,
-    unselectItem,
+    withUnselectItem,
   ]);
 
   const selectedYearLabel = useMemo(() => (selectedYear ? String(selectedYear) : undefined), [selectedYear]);
