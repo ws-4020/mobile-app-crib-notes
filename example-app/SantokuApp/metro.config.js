3cfc0015f7055395f224ee7ebe91b26e931ed4fd
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2022-05-18 17:32:19 +0900

    🔨: Handle `.license` and `.notice` files in `assets` directory as the React Native assets

--- example-app/SantokuApp/metro.config.js
+++ example-app/SantokuApp/metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require('expo/metro-config');
+const metroConfig = getDefaultConfig(__dirname);
 
-module.exports = getDefaultConfig(__dirname);
+// ライセンスファイルを拡張子license, noticeとしてassetディレクトリに保存しているので、それらのファイルがアセットとして扱われるように設定
+metroConfig.resolver.assetExts.push('license');
+metroConfig.resolver.assetExts.push('notice');
+
+module.exports = metroConfig;
