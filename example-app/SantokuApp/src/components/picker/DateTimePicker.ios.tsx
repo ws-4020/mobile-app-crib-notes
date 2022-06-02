871c7d4ebd839dc246739a65bde7ad806a1b1de0
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-06-02 12:00:12 +0900

    Localizationの設定についてコメント追記

--- example-app/SantokuApp/src/components/picker/DateTimePicker.ios.tsx
+++ example-app/SantokuApp/src/components/picker/DateTimePicker.ios.tsx
@@ -14,6 +14,27 @@ type DateTimePickerIOSProps = Omit<DateTimePickerProps, 'displayAndroid' | 'pick
     'value' | 'onChange' | 'mode' | 'display' | 'maximumDate' | 'minimumDate'
   >;
 };
+/**
+ * 日時を表示するPickerとしてReact Native DateTimePickerを使用しています。
+ * React Native DateTimePickerは、NativeのUIDatePickerを使用しています。
+ * UIDatePickerは、↓に記載されている「Support for Languages and Regions」の設定を追加しないと、OSに設定されている言語・地域に従った表示になりません。
+ * https://developer.apple.com/documentation/xcode/adding-support-for-languages-and-regions
+ *
+ * 言語・地域を追加する大まかな手順は以下になります。
+ * - Add Localizations
+ *   - Xcodeを開いて、PROJECT -> Info ->LocalizationsにLocalizationを追加
+ * - View Localizable Resources
+ *   - Xcodeを開いて、左ペインのナビゲータエリアでTargetを選択
+ *   - New FileでStrings Fileを「InfoPlist」というファイル名で作成（拡張子の.stringsは自動で付与されます）
+ *   - 作成した「InfoPlist」を選択して、右ペインのLocalization -> Localize...をタップ
+ *   - 対応するLocalizationを選択
+ *
+ * 上記を実行すると、「en.lproj」、「ja.lproj」のようなLocalization毎のディレクトリが作成され、それぞれのディレクトリの中にInfoPlist.stringsファイルが作成されます。
+ * InfoPlist.stringsの中身は特に何も定義されていない空のファイルですが、UIDatePickerを使用するだけであれば定義の追加は必要ありません。
+ *
+ * 参考までに、このアプリでLocalizationの対応を実施したPRのリンクを記載します。
+ * https://github.com/ws-4020/mobile-app-crib-notes/pull/861
+ */
 export const DateTimePicker = (props: DateTimePickerIOSProps) => {
   const {
     isVisible,
