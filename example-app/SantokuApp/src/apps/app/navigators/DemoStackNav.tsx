b8f7b115aee1b21bc4c4f07036443df32edf8cd6
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-07-26 15:37:26 +0900

    remove demo barrel

--- example-app/SantokuApp/src/apps/app/navigators/DemoStackNav.tsx
+++ example-app/SantokuApp/src/apps/app/navigators/DemoStackNav.tsx
@@ -1,43 +1,41 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {
-  DemoScreen,
-  AppStateScreen,
-  ConfigScreen,
-  ErrorCaseScreen,
-  ErrorInEventHandlerScreen,
-  ErrorInUseEffectScreen,
-  ErrorInUseEffectSyncProcessScreen,
-  ErrorInUseEffectAsyncProcessScreen,
-  ErrorInReactComponentScreen,
-  ErrorInNativeModuleScreen,
-  InstructionsScreen,
-  AppInfoScreen,
-  LogScreen,
-  MessageScreen,
-  ButtonScreen,
-  SnackbarScreen,
-  LocalAuthenticationScreen,
-  AuthenticationScreen,
-  HttpApiScreen,
-  NavigationScreen,
-  PushNotificationScreen,
-  CacheScreen,
-  ListTodoDemoScreen,
-  CreateTodoDemoScreen,
-  EditTodoDemoScreen,
-  ReactQueryDemoScreen,
-  DisabledQueryDemoScreen,
-  DependentQueryDemo1Screen,
-  DependentQueryDemo2Screen,
-  DependentQueryDemo3Screen,
-  DisableErrorHandlerDemoScreen,
-  GetAccountsMeDemoScreen,
-  SearchFormTodoDemoScreen,
-  SearchBarTodoDemoScreen,
-  PickerScreen,
-  AcknowledgementsScreen,
-  LicenseScreen,
-} from 'features';
+import {DemoScreen} from 'features/demo/DemoScreen';
+import {AcknowledgementsScreen} from 'features/demo/acknowledgements/AcknowledgementsScreen';
+import {LicenseScreen} from 'features/demo/acknowledgements/LicenseScreen';
+import {AppStateScreen} from 'features/demo/app-state/AppStateScreen';
+import {AuthenticationScreen} from 'features/demo/authentication/AuthenticationScreen';
+import {ButtonScreen} from 'features/demo/button/ButtonScreen';
+import {CacheScreen} from 'features/demo/cache/CacheScreen';
+import {ConfigScreen} from 'features/demo/config/ConfigScreen';
+import {ErrorCaseScreen} from 'features/demo/error/ErrorCaseScreen';
+import {ErrorInEventHandlerScreen} from 'features/demo/error/ErrorInEventHandlerScreen';
+import {ErrorInNativeModuleScreen} from 'features/demo/error/ErrorInNativeModuleScreen';
+import {ErrorInReactComponentScreen} from 'features/demo/error/ErrorInReactComponentScreen';
+import {ErrorInUseEffectAsyncProcessScreen} from 'features/demo/error/ErrorInUseEffectAsyncProcessScreen';
+import {ErrorInUseEffectScreen} from 'features/demo/error/ErrorInUseEffectScreen';
+import {ErrorInUseEffectSyncProcessScreen} from 'features/demo/error/ErrorInUseEffectSyncProcessScreen';
+import {HttpApiScreen} from 'features/demo/http-api/HttpApiScreen';
+import {AppInfoScreen} from 'features/demo/info/AppInfoScreen';
+import {InstructionsScreen} from 'features/demo/instructions/InstructionsScreen';
+import {LocalAuthenticationScreen} from 'features/demo/local-authn/LocalAuthenticationScreen';
+import {LogScreen} from 'features/demo/log/LogScreen';
+import {MessageScreen} from 'features/demo/message/MessageScreen';
+import {NavigationScreen} from 'features/demo/navigation/NavigationScreen';
+import {PickerScreen} from 'features/demo/picker/PickerScreen';
+import {PushNotificationScreen} from 'features/demo/push-notification/PushNotificationScreen';
+import {CreateTodoDemoScreen} from 'features/demo/react-query/CreateTodoDemoScreen';
+import {DependentQueryDemo1Screen} from 'features/demo/react-query/DependentQueryDemo1Screen';
+import {DependentQueryDemo2Screen} from 'features/demo/react-query/DependentQueryDemo2Screen';
+import {DependentQueryDemo3Screen} from 'features/demo/react-query/DependentQueryDemo3Screen';
+import {DisableErrorHandlerDemoScreen} from 'features/demo/react-query/DisableErrorHandlerDemoScreen';
+import {DisabledQueryDemoScreen} from 'features/demo/react-query/DisabledQueryDemoScreen';
+import {EditTodoDemoScreen} from 'features/demo/react-query/EditTodoDemoScreen';
+import {GetAccountsMeDemoScreen} from 'features/demo/react-query/GetAccountsMeDemoScreen';
+import {ListTodoDemoScreen} from 'features/demo/react-query/ListTodoDemoScreen';
+import {ReactQueryDemoScreen} from 'features/demo/react-query/ReactQueryDemoScreen';
+import {SearchBarTodoDemoScreen} from 'features/demo/react-query/SearchBarTodoDemoScreen';
+import {SearchFormTodoDemoScreen} from 'features/demo/react-query/SearchFormTodoDemoScreen';
+import {SnackbarScreen} from 'features/demo/snackbar/SnackbarScreen';
 import React from 'react';
 
 import {DemoStackParamList, RootStackParamList} from './types';
