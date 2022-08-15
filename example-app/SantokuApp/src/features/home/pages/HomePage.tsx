1028057fc3a63a24b71ce4a9ebd037e1e9950165
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-08-15 18:13:05 +0900

    refactor home

--- example-app/SantokuApp/src/features/home/pages/HomePage.tsx
+++ example-app/SantokuApp/src/features/home/pages/HomePage.tsx
@@ -1,12 +1,38 @@
-import React, {useEffect} from 'react';
+import {isGetFcmTokenError} from 'bases/firebase/messaging/getFcmToken';
+import {isRequestPushPermissionError} from 'bases/firebase/messaging/requestPushPermission';
+import {log} from 'bases/logging';
+import {m} from 'bases/message/Message';
+import {Snackbar} from 'bases/ui/snackbar/Snackbar';
+import React, {useCallback, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 
-import {useRequestPermissionAndRegisterToken} from '../use-cases/useRequestPermissionAndRegisterToken';
+import {useRequestPermissionAndRegisterToken} from '../services/useRequestPermissionAndRegisterToken';
 
 export const HomePage: React.VFC = () => {
+  const {requestPermissionAndRegisterToken: callRequestPermissionAndRegisterToken} =
+    useRequestPermissionAndRegisterToken();
   // Push通知の許可ダイアログ表示とFCM登録トークンの登録・更新
-  const {requestPermissionAndRegisterToken} = useRequestPermissionAndRegisterToken();
+  const requestPermissionAndRegisterToken = useCallback(async () => {
+    try {
+      await callRequestPermissionAndRegisterToken();
+    } catch (e) {
+      // Push通知のパーミッションリクエストに失敗した場合、Firebase Crashlyticsにエラーログを送信して、ユーザにはスナックバーでPush通知の受信ができない旨を伝える
+      if (isRequestPushPermissionError(e)) {
+        log.error(
+          m('app.push.notification.requestPushPermissionError', String(e)),
+          'app.push.notification.requestPushPermissionError',
+        );
+        Snackbar.show(m('app.push.notification.プッシュ通知の設定エラー'));
+      }
+      // FCMの登録トークンの取得に失敗した場合、Firebase Crashlyticsにエラーログを送信して、ユーザにはスナックバーでPush通知の受信ができない旨を伝える
+      if (isGetFcmTokenError(e)) {
+        log.error(m('app.push.notification.getFcmTokenError', String(e)), 'app.push.notification.getFcmTokenError');
+        Snackbar.show(m('app.push.notification.プッシュ通知の設定エラー'));
+      }
+    }
+  }, [callRequestPermissionAndRegisterToken]);
+
   useEffect(() => {
     requestPermissionAndRegisterToken().catch(() => {
       // requestPermissionAndRegisterTokenでエラーハンドリングを実施しているので、ここでは特に何もしない
