4263fcdef3680d6085dc3a5d702a59d720c971f8
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-08-15 15:33:58 +0900

    refactor demo-snackbar

--- example-app/SantokuApp/src/features/demo-snackbar/pages/SnackbarPage.tsx
+++ example-app/SantokuApp/src/features/demo-snackbar/pages/SnackbarPage.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import {m} from 'bases/message/Message';
+import {Snackbar} from 'bases/ui/snackbar/Snackbar';
+import React, {useCallback} from 'react';
 
 import {SnackbarTemplate} from '../components/SnackbarTemplate';
-import {useShowSnackbar} from '../use-cases/useShowSnackbar';
-import {useShowSnackbarWithCloseButton} from '../use-cases/useShowSnackbarWithCloseButton';
 
 export const SnackbarPage: React.FC = () => {
-  const {showSnackbar} = useShowSnackbar();
-  const {showSnackbarWithCloseButton} = useShowSnackbarWithCloseButton();
+  const showSnackbar = useCallback(() => {
+    Snackbar.show(m('app.webview.onError'));
+  }, []);
+
+  const showSnackbarWithCloseButton = useCallback(() => {
+    Snackbar.showWithCloseButton(m('app.webview.onError'));
+  }, []);
   return (
     <SnackbarTemplate
       testID="SnackbarScreen"
