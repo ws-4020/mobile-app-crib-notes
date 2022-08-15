aa43b631f39a04afd2bc65487acec2f6a8ff2db0
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-08-15 13:40:41 +0900

    refactor demo-app-state

--- example-app/SantokuApp/src/features/demo-app-state/client-states/useAppStateHistory.ts
+++
@@ -1,7 +0,0 @@
-import {useClientState} from 'bases/react-query/useClientState';
-
-import {StateChangedEventRecord} from '../types/StateChangedEventRecord';
-
-export const useAppStateHistory = () => {
-  return useClientState<StateChangedEventRecord[]>(['demo-app-state', 'appStateHistory'], []);
-};
--- example-app/SantokuApp/src/features/demo-app-state/pages/AppStatePage.tsx
+++ example-app/SantokuApp/src/features/demo-app-state/pages/AppStatePage.tsx
@@ -1,14 +1,47 @@
-import React, {useEffect} from 'react';
+import {useIsMounted} from 'bases/core/utils/useIsMounted';
+import React, {useCallback, useEffect, useState} from 'react';
+import {AppState, AppStateEvent, AppStateStatus, Platform} from 'react-native';
 
-import {useAppStateHistory} from '../client-states/useAppStateHistory';
 import {AppStateTemplate} from '../components/AppStateTemplate';
-import {useRegisterEventListeners} from '../use-cases/useRegisterEventListeners';
+import {StateChangedEventRecord} from '../types/StateChangedEventRecord';
 
 export const keyExtractor = (_: unknown, index: number) => index.toString();
 
 export const AppStatePage: React.FC = () => {
-  const [appStateHistory] = useAppStateHistory();
-  const {registerEventListeners} = useRegisterEventListeners();
+  const isMounted = useIsMounted();
+  const [appStateHistory, setAppStateHistory] = useState<StateChangedEventRecord[]>([]);
+  const saveEvent = useCallback(
+    (event: AppStateEvent, next: AppStateStatus) => {
+      if (isMounted()) {
+        setAppStateHistory(history => {
+          return [...(history ?? []), {event, timestamp: new Date(), state: next}];
+        });
+      }
+    },
+    [isMounted, setAppStateHistory],
+  );
+
+  const registerEventListener = useCallback(
+    (event: AppStateEvent) => {
+      if (Platform.OS !== 'android' && (event === 'focus' || event === 'blur')) {
+        return;
+      }
+
+      const listener = (next: AppStateStatus) => saveEvent(event, next);
+      const subscription = AppState.addEventListener(event, listener);
+      return () => subscription.remove();
+    },
+    [saveEvent],
+  );
+
+  const registerEventListeners = useCallback(() => {
+    const states: AppStateEvent[] = ['focus', 'blur', 'change', 'memoryWarning'];
+    const unregisterEventListeners = states.map(event => registerEventListener(event));
+    return () => {
+      unregisterEventListeners.forEach(unregister => unregister?.());
+    };
+  }, [registerEventListener]);
+
   useEffect(() => registerEventListeners(), [registerEventListeners]);
 
   return <AppStateTemplate testID="AppStateScreen" history={appStateHistory} keyExtractor={keyExtractor} />;
