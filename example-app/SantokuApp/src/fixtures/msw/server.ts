601ad1bb7bae4519a84f62c9650db9d3bace3ceb
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-08-16 19:46:31 +0900

    change onUnhandledRequest to req.passthrough

--- example-app/SantokuApp/src/fixtures/msw/server.ts
+++ example-app/SantokuApp/src/fixtures/msw/server.ts
@@ -5,7 +5,12 @@ import {handlers} from './handlers';
 export const initialServer = () => {
   const server = setupServer(...handlers);
   // setupServerでハンドリングしていないリクエストは、デフォルトではwarnログが出力されるため、Metroサーバへのリクエスト時に常にwarnログが出力されてしまいます。
-  // そのため、onUnhandledRequestにbypassを設定して、ハンドリングしていないリクエスト時はMSWでは何も実施しないようにします。
+  // そのため、ハンドリングしていないリクエスト時はMSWでは何も実施しないようにします。
   // https://mswjs.io/docs/api/setup-server/listen#onunhandledrequest
-  server.listen({onUnhandledRequest: 'bypass'});
+  // なお、onUnhandledRequestにbypassを設定しても同様のことができそうですが、リリースモードではバイパスされなかったため、明示的にreq.passthroughを呼び出しています。
+  server.listen({
+    onUnhandledRequest(req) {
+      req.passthrough();
+    },
+  });
 };
