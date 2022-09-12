555ff2e39446e947b897e21f6c67b8a70abce131
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-09-12 16:29:47 +0900

    fix format2Iso8601

--- example-app/SantokuApp/src/fixtures/msw/utils/format2Iso8601.ts
+++ example-app/SantokuApp/src/fixtures/msw/utils/format2Iso8601.ts
@@ -1,8 +1,3 @@
 export const format2Iso8601 = (d: Date): string => {
-  const month = String(d.getMonth() + 1).padStart(2, '0');
-  const date = String(d.getDate()).padStart(2, '0');
-  const hours = String(d.getHours()).padStart(2, '0');
-  const minutes = String(d.getMinutes()).padStart(2, '0');
-  const seconds = String(d.getSeconds()).padStart(2, '0');
-  return `${d.getFullYear()}-${month}-${date}T${hours}:${minutes}:${seconds}Z`;
+  return d.toISOString();
 };
