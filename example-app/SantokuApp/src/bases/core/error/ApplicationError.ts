5a9ab38ff53cb84ebe2124f64c671b446e051fdf
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-09-22 16:25:07 +0900

    Add errorCode to ApplicationError and RuntimeError

--- example-app/SantokuApp/src/bases/core/error/ApplicationError.ts
+++ example-app/SantokuApp/src/bases/core/error/ApplicationError.ts
@@ -1,6 +1,28 @@
 import {ErrorWrapper} from './ErrorWrapper';
 
-export class ApplicationError extends ErrorWrapper {}
+export class ApplicationError extends ErrorWrapper {
+  constructor();
+  constructor(cause: unknown);
+  constructor(message: string);
+  constructor(cause: unknown, errorCode: string);
+  constructor(message: string, errorCode: string);
+  constructor(message: string, cause: unknown, errorCode: string);
+  constructor(messageOrCause?: unknown, cause?: unknown, errorCode?: string) {
+    if (typeof messageOrCause === 'string') {
+      super(messageOrCause, cause);
+      this._errorCode = errorCode;
+    } else {
+      super(messageOrCause);
+      this._errorCode = errorCode;
+    }
+  }
+
+  private readonly _errorCode?: string;
+
+  get errorCode() {
+    return this._errorCode;
+  }
+}
 
 export function isApplicationError(error?: any): error is ApplicationError {
   return error != null && typeof error === 'object' && error instanceof ApplicationError;
--- example-app/SantokuApp/src/bases/core/error/RuntimeError.ts
+++ example-app/SantokuApp/src/bases/core/error/RuntimeError.ts
@@ -1,6 +1,28 @@
 import {ErrorWrapper} from './ErrorWrapper';
 
-export class RuntimeError extends ErrorWrapper {}
+export class RuntimeError extends ErrorWrapper {
+  constructor();
+  constructor(cause: unknown);
+  constructor(message: string);
+  constructor(cause: unknown, errorCode: string);
+  constructor(message: string, errorCode: string);
+  constructor(message: string, cause: unknown, errorCode: string);
+  constructor(messageOrCause?: unknown, cause?: unknown, errorCode?: string) {
+    if (typeof messageOrCause === 'string') {
+      super(messageOrCause, cause);
+      this._errorCode = errorCode;
+    } else {
+      super(messageOrCause);
+      this._errorCode = errorCode;
+    }
+  }
+
+  private readonly _errorCode?: string;
+
+  get errorCode() {
+    return this._errorCode;
+  }
+}
 
 export function isRuntimeError(error?: any): error is RuntimeError {
   return error != null && typeof error === 'object' && error instanceof RuntimeError;
