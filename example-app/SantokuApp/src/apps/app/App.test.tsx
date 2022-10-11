710cdb4b7060a2c45ade8471269edc160ea6fe1a
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-10-11 14:20:35 +0900

    fix App.test

--- example-app/SantokuApp/src/apps/app/App.test.tsx
+++ example-app/SantokuApp/src/apps/app/App.test.tsx
@@ -1,89 +1,16 @@
-import * as Application from 'expo-application';
-import * as SecureStore from 'expo-secure-store';
 import '@testing-library/jest-native/extend-expect';
 import {render, screen, waitFor} from '@testing-library/react-native';
-import {BACKEND_AXIOS_INSTANCE_WITHOUT_REFRESH_SESSION} from 'features/backend/utils/customInstance';
 import {initialData} from 'fixtures/msw/datas';
+import {initialDb} from 'fixtures/msw/db';
+import {handlers} from 'fixtures/msw/handlers';
 import {setupServer} from 'msw/node';
 import React from 'react';
 
-import {initialDb} from '../../fixtures/msw/db';
-import {handlers} from '../../fixtures/msw/handlers';
 import {App} from './App';
 
-// jest.mock('react-native/Libraries/Utilities/DevSettings.js', () => {
-//   return {addMenuItem: jest.fn};
-// });
-// jest.spyOn(BACKEND_AXIOS_INSTANCE_WITHOUT_REFRESH_SESSION, 'get').mockResolvedValue({
-//   status: 200,
-//   data: {
-//     csrfTokenHeaderName: 'X-CSRF-TOKEN',
-//     csrfTokenValue: 'dummy',
-//   },
-// });
-// jest.mock('features/backend/apis/system/system', () => {
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//   const originalModule = jest.requireActual('features/backend/apis/system/system');
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-//   return {
-//     ...originalModule,
-//     getAppUpdates: jest.fn(() => {
-//       return Promise.resolve({
-//         status: 200,
-//         data: {
-//           updateRequired: false,
-//           supportedVersion: 'dummy',
-//           message: 'dummy',
-//         },
-//       });
-//     }),
-//   };
-// });
-//
-// jest.mock('features/backend/apis/account/account', () => {
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//   const originalModule = jest.requireActual('features/backend/apis/account/account');
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-//   return {
-//     ...originalModule,
-// getAccountsMe: jest.fn(() => {
-//   return Promise.resolve({
-//     status: 200,
-//     data: {
-//       accountId: '123456789',
-//       deviceTokens: [],
-//     },
-//   });
-// }),
-// getAccountsMeTerms: jest.fn(() => {
-//   return Promise.resolve({
-//     status: 200,
-//     data: {
-//       hasAgreed: true,
-//       agreedVersion: '1.0.0',
-//     },
-//   });
-// }),
-//     postLogin: jest.fn(() => {
-//       return Promise.resolve({
-//         status: 200,
-//         data: {
-//           status: 'COMPLETE',
-//         },
-//       });
-//     }),
-//   };
-// });
-//
-// jest.mock('features/backend/apis/terms/terms', () => {
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//   const originalModule = jest.requireActual('features/backend/apis/terms/terms');
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-//   return {
-//     ...originalModule,
-//     getTerms: jest.fn(),
-//   };
-// });
+jest.mock('react-native/Libraries/Utilities/DevSettings.js', () => {
+  return {addMenuItem: jest.fn};
+});
 
 const server = setupServer(...handlers);
 beforeAll(async () => {
@@ -115,7 +42,7 @@ jest.mock('expo-secure-store', () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return {
     ...originalModule,
-    getItemAsync: jest.fn((key: string) => {
+    getItemAsync: jest.fn(() => {
       return Promise.resolve('santoku');
     }),
   };
