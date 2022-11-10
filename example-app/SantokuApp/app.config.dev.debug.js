f7c4594b44b623dfa49afa743248b8fd178aa5f7
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-11-10 18:05:11 +0900

    refactor build variant

--- example-app/SantokuApp/app.config.dev.debug.js
+++ example-app/SantokuApp/app.config.dev.debug.js
@@ -1,20 +1,3 @@
 module.exports = config => {
-  return {
-    ...config,
-    name: 'Dev SantokuApp',
-    android: {
-      ...config.android,
-      package: 'jp.fintan.mobile.SantokuApp.dev.debug',
-      adaptiveIcon: {
-        ...config.android.adaptiveIcon,
-        foregroundImage: './assets/ic_launcher_foreground_debug.png',
-      },
-      googleServicesFile: './google-services-dummy.json',
-    },
-    extra: {
-      ...config.extra,
-      mobileAppCribNotesWebsiteUrl: 'https://ws-4020.github.io/mobile-app-crib-notes',
-      mobileAppCribNotesRepositoryUrl: 'https://github.com/ws-4020/mobile-app-crib-notes',
-    },
-  };
+  return config;
 };
