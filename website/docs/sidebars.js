31e993a94e9b1cbc5e8658cf0bb001afae107c52
kozake <sh.kozake@gmail.com>
2023-03-14 19:06:58 +0900

    reverse order

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -632,8 +632,8 @@ module.exports = {
           label: 'エンハンス',
           items: [
             'react-native/santoku/maintenance/enhance',
-            'react-native/santoku/maintenance/enhance/expo-46-upgrade',
             'react-native/santoku/maintenance/enhance/config-plugins',
+            'react-native/santoku/maintenance/enhance/expo-46-upgrade',
           ],
         },
       ],
