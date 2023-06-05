458358bdf3d38f1214a69bafaf05a9724ca617fb
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-06-05 11:48:11 +0900

    change MarkerProps to MapMarkerProps
    ref: react-native-maps/react-native-maps@fd798c9826a9edfa936716e8ed78d40c07ccb9c7
    fix(types): export types that were present before v1

--- example-app/SantokuApp/src/bases/ui/map/Marker.tsx
+++ example-app/SantokuApp/src/bases/ui/map/Marker.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Marker as RNMMarker, MarkerProps as RNMMarkerProps} from 'react-native-maps';
+import {Marker as RNMMarker, MapMarkerProps as RNMapMarkerProps} from 'react-native-maps';
 
 // iOSでGoogle Mapのみで指定できるProps（tappable, tracksInfoWindowChanges）を削除する
-export type MarkerProps = Omit<RNMMarkerProps, 'tappable' | 'tracksInfoWindowChanges'>;
+export type MarkerProps = Omit<RNMapMarkerProps, 'tappable' | 'tracksInfoWindowChanges'>;
 
 export const Marker: React.FC<MarkerProps> = (props: MarkerProps) => {
   return <RNMMarker {...props} />;
--- example-app/SantokuApp/src/features/demo-map/constants/initialMarker.ts
+++ example-app/SantokuApp/src/features/demo-map/constants/initialMarker.ts
@@ -1,6 +1,6 @@
-import {MarkerProps} from 'react-native-maps';
+import {MapMarkerProps} from 'react-native-maps';
 
-export const initialMarker: MarkerProps = {
+export const initialMarker: MapMarkerProps = {
   coordinate: {latitude: 34.7024898, longitude: 135.4937566},
   title: '大阪駅',
   description: '大阪府大阪市北区梅田３丁目１−１',
