e5dc701ca6662e014bdf06d5140bf970a67e4934
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-06-14 17:39:09 +0900

    [mobile] getFcmToken.ts内の関数をuseGetFcmToken.tsに移動

--- example-app/SantokuApp/src/features/demo-push-notification/services/useGetFcmToken.ts
+++ example-app/SantokuApp/src/features/demo-push-notification/services/useGetFcmToken.ts
@@ -1,6 +1,9 @@
+import messaging from '@react-native-firebase/messaging';
 import {useQuery} from 'react-query';
 
-import {getFcmToken} from './getFcmToken';
+const getFcmToken = () => {
+  return messaging().getToken();
+};
 
 export const useGetFcmToken = () => {
   const query = useQuery(['demo', 'push-notification', 'getFcmToken'], getFcmToken);
