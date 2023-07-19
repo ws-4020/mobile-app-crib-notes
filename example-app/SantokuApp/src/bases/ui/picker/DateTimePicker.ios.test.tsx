ef7aeb0214f88fc17caae1d58a30d600e6b22c7a
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-07-19 15:36:18 +0900

    ts-expect-errorの説明文の前に「--」を追加

--- example-app/SantokuApp/src/bases/ui/picker/DateTimePicker.ios.test.tsx
+++ example-app/SantokuApp/src/bases/ui/picker/DateTimePicker.ios.test.tsx
@@ -159,7 +159,7 @@ describe('DateTimePicker with all props', () => {
     expect(pickerItemsProps.maximumDate).toBe(maximumDate.getTime());
     expect(pickerItemsProps.minimumDate).toBe(minimumDate.getTime());
     expect(pickerItemsProps.mode).toBe('date');
-    // @ts-expect-error displayはdisplayIOSというPropでレンダリングされます
+    // @ts-expect-error -- displayはdisplayIOSというPropでレンダリングされます
     expect(pickerItemsProps.displayIOS).toBe('spinner');
     const pickerItemsStyle = pickerItemsProps.style as ViewStyle;
     const flattenPickerItemsStyle = StyleSheet.flatten(pickerItemsStyle);
