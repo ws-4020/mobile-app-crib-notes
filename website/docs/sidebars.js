54fe713bb335518ff7975b1005e9df7330f63633
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-08-10 17:37:08 +0900

    Android App Linksページを追加

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -383,6 +383,7 @@ module.exports = {
           items: [
             'react-native/santoku/application-architecture/deep-link/overview',
             'react-native/santoku/application-architecture/deep-link/universal-links',
+            'react-native/santoku/application-architecture/deep-link/android-app-links',
             'react-native/santoku/application-architecture/deep-link/process-deep-link',
           ],
         },
