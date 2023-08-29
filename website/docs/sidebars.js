ad7ef47c069e706b5e89e16f8a73c3de27b6f9dd
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-08-29 17:39:03 +0900

    Universal Links/Android App Linksに環境ごとの設定方法を追加

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -382,6 +382,8 @@ module.exports = {
           label: 'ディープリンク',
           items: [
             'react-native/santoku/application-architecture/deep-link/overview',
+            'react-native/santoku/application-architecture/deep-link/universal-links',
+            'react-native/santoku/application-architecture/deep-link/android-app-links',
             'react-native/santoku/application-architecture/deep-link/process-deep-link',
           ],
         },
