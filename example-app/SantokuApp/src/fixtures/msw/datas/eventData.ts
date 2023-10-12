0d66f0744b68955f446abf4eab528fde0db40c80
sinho-lee <i.shinho.695@tis.co.jp>
2023-10-12 11:36:24 +0900

    モックデータのイベント日付を未来にする

--- example-app/SantokuApp/src/fixtures/msw/datas/eventData.ts
+++ example-app/SantokuApp/src/fixtures/msw/datas/eventData.ts
@@ -4,6 +4,8 @@ import {partnerAccount} from './accountMinData';
 import {db} from '../db';
 
 export const eventData = () => {
+  const date = new Date();
+  date.setDate(date.getDate() + 1);
   db.event.create({
     eventId: '1',
     title: '【勉強会】今話題の技術を触ってみよう',
@@ -13,7 +15,7 @@ export const eventData = () => {
 ■参加資格
 制限なし
 `,
-    endDate: '2022-06-01T10:00:00Z',
+    endDate: date.toString(),
     likes: 1,
     ...santokuAccount,
   });
@@ -29,7 +31,7 @@ export const eventData = () => {
 ■概要
 マスタリングTCP/IP 入門を読みながら、不明点などを議論していくスタイルです。
 `,
-    endDate: '2022-06-03T10:00:00Z',
+    endDate: date.toString(),
     likes: 3,
     ...santokuAccount,
   });
@@ -46,7 +48,7 @@ export const eventData = () => {
 React NativeのNew Architectureは、さまざまな理由により導入に踏み切ってない方も多いかと思います。
 まだまだ少ない事例の中、実際にNew Architectureを導入した開発者に、導入して感じたことを話して頂きます。
 `,
-    endDate: '2022-06-10T10:00:00Z',
+    endDate: date.toString(),
     likes: 10,
     ...adminAccount,
   });
@@ -62,7 +64,7 @@ React NativeのNew Architectureは、さまざまな理由により導入に踏�
 ■概要
 Keycloakを使用しながら、実際にOpen ID Connectの各種フローを追っていきます。
 `,
-    endDate: '2023-01-18T10:00:00Z',
+    endDate: date.toString(),
     likes: 5,
     ...santokuAccount,
   });
@@ -78,7 +80,7 @@ Keycloakを使用しながら、実際にOpen ID Connectの各種フローを追
 ■概要
 ReactとFirebaseを使用してWebアプリケーションを構築した開発者が、そのメリットやデメリットなどを話します。
 `,
-    endDate: '2023-02-01T10:00:00Z',
+    endDate: date.toString(),
     likes: 8,
     ...santokuAccount,
   });
@@ -92,7 +94,7 @@ ReactとFirebaseを使用してWebアプリケーションを構築した開発�
 ■参加資格
 XXRが全くわからない方
 `,
-    endDate: '2023-06-07T10:00:00Z',
+    endDate: date.toString(),
     likes: 15,
     ...partnerAccount,
   });
@@ -109,7 +111,7 @@ XXRが全くわからない方
 不具合が発生した場合に、みなさんはどのようにログを追っていますか？
 今回のイベントは、Cloud Nativeのトレーサビリティに関する話を中心とした催しになります。
 `,
-    endDate: '2023-07-17T10:00:00Z',
+    endDate: date.toString(),
     likes: 18,
     ...adminAccount,
   });
