58ff4d46e8c72a95f22b660356743845add1158a
sinho-lee <i.shinho.695@tis.co.jp>
2023-10-16 13:50:50 +0900

    reactjs.orgリンクをリダイレクトが無いように変更する

--- example-app/SantokuApp/src/bases/core/utils/usePrevious.ts
+++ example-app/SantokuApp/src/bases/core/utils/usePrevious.ts
@@ -3,7 +3,7 @@ import {useEffect, useRef} from 'react';
 /**
  * This React hook provides the previous value. (To be precise, it returns the previous reference.)
  *
- * See [Hooks FAQ - How to get the previous props or state?](https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state).
+ * See [Hooks FAQ - How to get the previous props or state?](https://legacy.reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state).
  */
 export const usePrevious = <T>(current: T) => {
   const ref = useRef<T>();
