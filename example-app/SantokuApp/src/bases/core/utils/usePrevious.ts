68a2fa1fcb5433cb8777747a9a4623a8e36adf23
sinho-lee <i.shinho.695@tis.co.jp>
2023-10-20 17:45:49 +0900

    legacyリンク追加及びコメント追加

--- example-app/SantokuApp/src/bases/core/utils/usePrevious.ts
+++ example-app/SantokuApp/src/bases/core/utils/usePrevious.ts
@@ -3,12 +3,18 @@ import {useEffect, useRef} from 'react';
 /**
  * This React hook provides the previous value. (To be precise, it returns the previous reference.)
  *
- * We have previously suggested a custom Hook called usePrevious to hold the previous value.
- * However, we’ve found that most use cases fall into the two patterns described above.
- * If your use case is different, you can hold a value in a ref and manually update it when needed.
- * Avoid reading and updating refs during rendering because this makes your component’s behavior difficult to predict and understand.
+ * 基本は、usePreviousを使用しないパターンで実装するべき
  *
- * See [Storing information from previous renders](https://ja.react.dev/reference/react/useState#storing-information-from-previous-renders).
+ * どうしてもRefを使う必要がある場面だけusePreviousを使用する
+ *
+ * See [Hooks FAQ - How to get the previous props or state?](https://legacy.reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state).
+ *
+ * > We have previously suggested a custom Hook called usePrevious to hold the previous value.
+ * > However, we’ve found that most use cases fall into the two patterns described above.
+ * > If your use case is different, you can hold a value in a ref and manually update it when needed.
+ * > Avoid reading and updating refs during rendering because this makes your component’s behavior difficult to predict and understand.
+ *
+ * @See [Storing information from previous renders](https://ja.react.dev/reference/react/useState#storing-information-from-previous-renders).
  */
 export const usePrevious = <T>(current: T) => {
   const ref = useRef<T>();
