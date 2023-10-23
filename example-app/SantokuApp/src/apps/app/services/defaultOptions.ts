b49d19d06a7ff4f10c55d047a17b58bce38136cc
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-10-23 17:57:37 +0900

    networkModeにofflineFirstを設定している理由を追記
    Co-authored-by: Takashi Sugimoto <tksugimoto@users.noreply.github.com>

--- example-app/SantokuApp/src/apps/app/services/defaultOptions.ts
+++ example-app/SantokuApp/src/apps/app/services/defaultOptions.ts
@@ -1,6 +1,8 @@
 const defaultOptions = {
   queries: {
     retry: false, // default: 3
+    // Santokuでは、オフライン時のエラー処理をAxiosからエラーがthrowされるかで判定しており、onlineモードでQueryが発行されないとエラー処理が実施されないため
+    // https://tanstack.com/query/v4/docs/react/guides/migrating-to-react-query-4#queries-and-mutations-per-default-need-network-connection-to-run
     networkMode: 'offlineFirst' as const, // default: 'online'
   },
   mutations: {
