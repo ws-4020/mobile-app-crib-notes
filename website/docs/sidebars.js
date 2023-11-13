e14bdea5e0ee9d35d9737af6b1b4a8e52bbb8163
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-11-13 11:12:12 +0900

    React Nativeの紹介ページを追加

--- website/docs/sidebars.js
+++ website/docs/sidebars.js
@@ -115,6 +115,7 @@ module.exports = {
       label: 'アプリの実装前に',
       items: [
         'react-native/learn/basic-concepts',
+        'react-native/learn/basic-concepts/what-is-react-native',
         {
           type: 'category',
           label: 'React Nativeの基本',
