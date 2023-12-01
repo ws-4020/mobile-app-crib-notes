7cb93695a832f5c340c88000d9a53319100069d4
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2023-12-01 10:14:04 +0900

    Revert "プロジェクト内のjs、tsx、tsファイルにライセンスを追加"
    This reverts commit 64c43634584dea13bdb03a9bee5e2ce767e6675e.

--- example-app/SantokuApp/src/features/demo-instructions/pages/InstructionsPage.tsx
+++ example-app/SantokuApp/src/features/demo-instructions/pages/InstructionsPage.tsx
@@ -1,19 +1,3 @@
-/**
- * Copyright 2023 TIS Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /*
  * Original source: https://github.com/react-native-community/react-native-template-typescript/blob/60690d1f7f3c2856d4c7129fd972400452c9510d/template/App.tsx
  *
