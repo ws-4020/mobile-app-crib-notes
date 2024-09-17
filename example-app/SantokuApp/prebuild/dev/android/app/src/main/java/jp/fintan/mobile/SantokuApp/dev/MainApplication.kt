53582182520b6e23407403a337644fdc05cd6b93
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2024-09-17 16:23:43 +0900

    prebuild

--- example-app/SantokuApp/prebuild/dev/android/app/src/main/java/jp/fintan/mobile/SantokuApp/dev/MainApplication.kt
+++ example-app/SantokuApp/prebuild/dev/android/app/src/main/java/jp/fintan/mobile/SantokuApp/dev/MainApplication.kt
@@ -1,81 +1,65 @@
-package jp.fintan.mobile.SantokuApp.dev;
+package jp.fintan.mobile.SantokuApp.dev
 
-import android.app.Application;
-import android.content.res.Configuration;
-import androidx.annotation.NonNull;
+import android.app.Application
+import android.content.res.Configuration
+import androidx.annotation.NonNull
 
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.soloader.SoLoader;
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactHost
+import com.facebook.react.config.ReactFeatureFlags
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.soloader.SoLoader
 
-import expo.modules.ApplicationLifecycleDispatcher;
-import expo.modules.ReactNativeHostWrapper;
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
 
-import java.util.List;
+class MainApplication : Application(), ReactApplication {
 
-public class MainApplication extends Application implements ReactApplication {
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+        this,
+        object : DefaultReactNativeHost(this) {
+          override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages
+            packages.add(ThrowErrorPackage())
+            return packages
+          }
 
-  private final ReactNativeHost mReactNativeHost =
-    new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
-      @Override
-      public boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
+          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+
+          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
+  )
 
-      @Override
-      protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      packages.add(new ThrowErrorPackage());;
-        // Packages that cannot be autolinked yet can be added manually here, for example:
-        // packages.add(new MyReactNativePackage());
-        return packages;
-      }
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
-      @Override
-      protected String getJSMainModuleName() {
-        return ".expo/.virtual-metro-entry";
-      }
-
-      @Override
-      protected boolean isNewArchEnabled() {
-        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-      }
-
-      @Override
-      protected Boolean isHermesEnabled() {
-        return BuildConfig.IS_HERMES_ENABLED;
-      }
-  });
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
     if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false;
+      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
     }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
+      load()
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+    if (BuildConfig.DEBUG) {
+      ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
-  @Override
-  public void onConfigurationChanged(@NonNull Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
   }
 }
--- example-app/SantokuApp/prebuild/prod/android/app/src/main/java/jp/fintan/mobile/SantokuApp/MainActivity.kt
+++ example-app/SantokuApp/prebuild/prod/android/app/src/main/java/jp/fintan/mobile/SantokuApp/MainActivity.kt
@@ -1,49 +1,44 @@
-package jp.fintan.mobile.SantokuApp;
+package jp.fintan.mobile.SantokuApp
 
-import android.net.Uri;
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Set;
+import android.net.Uri
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 // Transition中にActivityのbackgroundが表示される問題があるため、Splash Screen用とReact Native用にActivityを分ける.
 // https://github.com/software-mansion/react-native-screens/issues/380
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        Intent mainIntent = getIntent();
-        Intent appIntent = new Intent(this, AppActivity.class);
+        val mainIntent = getIntent()
+        val appIntent = Intent(this, AppActivity::class.java)
 
         // Push通知で、onNotificationOpenedAppやgetInitialNotificationを使用してイベントを受け取れない事象の対応
         // https://github.com/invertase/react-native-firebase/issues/3469#issuecomment-614990736
         // Pass along FCM messages/notifications etc.
-        Bundle extras = mainIntent.getExtras();
+        var extras = mainIntent.getExtras()
         if (extras != null) {
-            appIntent.putExtras(extras);
+            appIntent.putExtras(extras)
         }
 
         // Pass along deep links etc.
-        String action = mainIntent.getAction();
-        Uri data = mainIntent.getData();
-        Set<String> categories = mainIntent.getCategories();
+        val action = mainIntent.getAction()
+        val data = mainIntent.getData()
+        val categories = mainIntent.getCategories()
         if (action != null) {
-            appIntent.setAction(action);
+            appIntent.setAction(action)
         }
         if (data != null) {
-            appIntent.setData(data);
+            appIntent.setData(data)
         }
         if (categories != null && !categories.isEmpty()) {
-            for (String category : categories) {
-                appIntent.addCategory(category);
+            for (category in categories) {
+                appIntent.addCategory(category)
             }
         }
 
-        startActivity(appIntent);
-        finish();
+        startActivity(appIntent)
+        finish()
     }
 }
