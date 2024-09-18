71a0dc648837bb9a2f05a436206066de92ce1392
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2024-09-18 21:44:53 +0900

    Pickerでkeyが数値(number, bigint)の場合にも正しく動作するように修正

--- example-app/SantokuApp/src/bases/ui/picker/useSelectPicker.ts
+++ example-app/SantokuApp/src/bases/ui/picker/useSelectPicker.ts
@@ -15,6 +15,7 @@
  */
 
 import React, {useCallback, useMemo, useState} from 'react';
+import {Platform} from 'react-native';
 
 import {SelectPickerProps} from './SelectPicker';
 
@@ -31,7 +32,15 @@ export const useSelectPicker = <ItemT>({
   const close = useCallback(() => setIsVisible(false), []);
   const getSelectedItem = useCallback(
     (key?: React.Key | ItemT) => {
-      return items.find(item => item.key === key || item.value === key);
+      return items.find(item => {
+        if (Platform.OS === 'ios') {
+          // @react-native-picker/pickerは、iOSではpickerに渡されたvalueを強制的に文字列化してしまいます。
+          // そのため、onValueChangeに渡されてくる値は文字列となり、valueが文字列でなかった場合は直接比較しても一致しません。
+          // ここでは、@react-native-picker/pickerの内部処理と同様に、双方を文字列化して比較することで、選択中の要素を取得します。
+          return String(item.key) === String(key) || String(item.value) === String(key);
+        }
+        return item.key === key || item.value === key;
+      });
     },
     [items],
   );
--- example-app/SantokuApp/src/features/demo-picker/components/DemoSelectPicker.tsx
+++ example-app/SantokuApp/src/features/demo-picker/components/DemoSelectPicker.tsx
@@ -25,17 +25,17 @@ type PickerItemType = {
   a: string;
 };
 const pickerItems: Item<PickerItemType>[] = [
-  {value: {a: '1'}, label: 'test1', key: '1', inputLabel: 'テスト1'},
-  {value: {a: '2'}, label: 'test2', key: '2', inputLabel: 'テスト2'},
-  {value: {a: '3'}, label: 'test3', key: '3', inputLabel: 'テスト3'},
-  {value: {a: '4'}, label: 'test4', key: '4', inputLabel: 'テスト4'},
-  {value: {a: '5'}, label: 'test5', key: '5', inputLabel: 'テスト5'},
-  {value: {a: '6'}, label: 'test6', key: '6', inputLabel: 'テスト6'},
-  {value: {a: '7'}, label: 'test7', key: '7', inputLabel: 'テスト7'},
-  {value: {a: '8'}, label: 'test8', key: '8', inputLabel: 'テスト8'},
-  {value: {a: '9'}, label: 'test9', key: '9', inputLabel: 'テスト9'},
-  {value: {a: '10'}, label: 'test10', key: '10', inputLabel: 'テスト10'},
-  {value: {a: '11'}, label: 'test11', key: '11', inputLabel: 'テスト11'},
+  {value: {a: '1'}, label: 'test1', key: 1, inputLabel: 'テスト1'},
+  {value: {a: '2'}, label: 'test2', key: 2n, inputLabel: 'テスト2'},
+  {value: {a: '3'}, label: 'test3', key: 3, inputLabel: 'テスト3'},
+  {value: {a: '4'}, label: 'test4', key: 4n, inputLabel: 'テスト4'},
+  {value: {a: '5'}, label: 'test5', key: 5, inputLabel: 'テスト5'},
+  {value: {a: '6'}, label: 'test6', key: 6n, inputLabel: 'テスト6'},
+  {value: {a: '7'}, label: 'test7', key: 7, inputLabel: 'テスト7'},
+  {value: {a: '8'}, label: 'test8', key: 8n, inputLabel: 'テスト8'},
+  {value: {a: '9'}, label: 'test9', key: 9, inputLabel: 'テスト9'},
+  {value: {a: '10'}, label: 'test10', key: 10n, inputLabel: 'テスト10'},
+  {value: {a: '11'}, label: 'test11', key: 11, inputLabel: 'テスト11'},
 ];
 
 export const DemoSelectPicker: React.FC = () => {
