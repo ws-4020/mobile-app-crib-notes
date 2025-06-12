6d0a0fe1439a08b66435d7b0767419c42286e374
nakamura.toru.021 <nakamura.toru.021@tis.co.jp>
2025-06-12 15:59:57 +0900

    修正

--- example-app/SantokuApp/src/fixtures/msw/handlers/event/getEvents.ts
+++ example-app/SantokuApp/src/fixtures/msw/handlers/event/getEvents.ts
@@ -23,7 +23,7 @@ import {getDb} from '../../utils/getDb';
 import {getLoggedInAccountId} from '../account/getLoggedInAccountId';
 
 export const getEvents = rest.get(`${backendUrl}/events`, (req, res, ctx) => {
-  console.log(req.headers);
+  console.log(JSON.stringify(req.headers));
   try {
     const target = req.url.searchParams.get('target');
     const accountId = getLoggedInAccountId();
